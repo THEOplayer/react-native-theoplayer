d9e10966d1cd21312a64807f11f6a3a0b0eb0f97
William Van Haevre <william.vanhaevre@gmail.com>
2022-10-11 17:31:09 +0200

    Bridge iOS general casting status

--- ios/THEOplayerRCTCastAPI.swift
+++ ios/THEOplayerRCTCastAPI.swift
@@ -9,9 +9,11 @@
 import Foundation
 import UIKit
 
+let ERROR_CODE_CAST_ACCESS_FAILURE = "cast_access_failure"
 let ERROR_MESSAGE_CASTING_UNSUPPORTED_FEATURE = "Chromecast and Airplay are not supported by the provided iOS SDK"
 let ERROR_MESSAGE_CHROMECAST_UNSUPPORTED_FEATURE = "Chromecast is not supported by the provided iOS SDK"
 let ERROR_MESSAGE_AIRPLAY_UNSUPPORTED_FEATURE = "Airplay is not supported by the provided iOS SDK"
+let ERROR_MESSAGE_CAST_ACCESS_FAILURE = "Could not access THEOplayer Cast Module"
 
 @objc(THEOplayerRCTCastAPI)
 class THEOplayerRCTCastAPI: NSObject, RCTBridgeModule {
@@ -31,8 +33,15 @@ class THEOplayerRCTCastAPI: NSObject, RCTBridgeModule {
     
     @objc(casting:resolver:rejecter:)
     func casting(_ node: NSNumber, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-        print("CastModule - casting: todo")
-        resolve(false)
+        DispatchQueue.main.async {
+            let theView = self.bridge.uiManager.view(forReactTag: node) as! THEOplayerRCTView
+            if let cast = theView.cast() {
+                resolve(cast.casting)
+            } else {
+                reject(ERROR_CODE_CAST_ACCESS_FAILURE, ERROR_MESSAGE_CAST_ACCESS_FAILURE, nil)
+                if DEBUG_ADS_API { print("[NATIVE] Could not retrieve current casting status (cast module unavailable).") }
+            }
+        }
     }
     
 #else
