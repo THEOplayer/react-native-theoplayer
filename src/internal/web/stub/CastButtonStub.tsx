bdbf70a1c0530e4cce28b45f8109b0c6adda489b
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2022-10-17 13:05:17 +0200

    Add castbutton stub for web

---
+++ src/internal/web/stub/CastButtonStub.tsx
@@ -0,0 +1,7 @@
+import type { ViewProps } from 'react-native';
+import React from 'react';
+
+// Provides a stub for react-native-google-cast on web.
+export function CastButton(_props: ViewProps): JSX.Element {
+  return <></>;
+}
