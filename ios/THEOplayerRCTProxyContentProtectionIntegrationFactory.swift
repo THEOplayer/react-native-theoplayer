ab8117c4b64fa5cf8024ce6e8f8a6aefa6fb533d
William Van Haevre <william.vanhaevre@gmail.com>
2022-11-04 18:25:39 +0100

    Add debug logs for proxy factory

--- ios/THEOplayerRCTProxyContentProtectionIntegrationFactory.swift
+++ ios/THEOplayerRCTProxyContentProtectionIntegrationFactory.swift
@@ -8,7 +8,7 @@
 import Foundation
 import THEOplayerSDK
 
-let PROXY_FACTORY_TAG: String = "[ProxyContentProtectionIntegrationFactory] "
+let PROXY_FACTORY_TAG: String = "[ProxyContentProtectionIntegrationFactory]"
 
 class THEOplayerRCTProxyContentProtectionIntegrationFactory: THEOplayerSDK.ContentProtectionIntegrationFactory {
     private weak var contentProtectionAPI: THEOplayerRCTContentProtectionAPI?
@@ -16,12 +16,14 @@ class THEOplayerRCTProxyContentProtectionIntegrationFactory: THEOplayerSDK.Conte
     private var keySystemId: String!
     
     init(contentProtectionAPI: THEOplayerRCTContentProtectionAPI, integrationId: String, keySystemId: String) {
+        if DEBUG_CONTENT_PROTECTION_API { print(PROXY_FACTORY_TAG, "Proxy ContentProtectionIntegrationFactory initialised for \(integrationId)-\(keySystemId).") }
         self.contentProtectionAPI = contentProtectionAPI
         self.integrationId = integrationId
         self.keySystemId = keySystemId
     }
     
     func build(configuration: THEOplayerSDK.DRMConfiguration) -> ContentProtectionIntegration {
+        if DEBUG_CONTENT_PROTECTION_API { print(PROXY_FACTORY_TAG, "Building proxy ContentProtectionIntegration for \(self.integrationId) - \(self.keySystemId).") }
         return THEOplayerRCTProxyContentProtectionIntegration(contentProtectionAPI: self.contentProtectionAPI,
                                                               integrationId: self.integrationId,
                                                               keySystemId: self.keySystemId,
