dba118a99b5da10edfc4a1474bff3b0fe745aa3f
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2022-11-09 16:21:37 +0100

    Add drm request time-out for Android

--- android/src/main/java/com/theoplayer/drm/ContentProtectionModule.kt
+++ android/src/main/java/com/theoplayer/drm/ContentProtectionModule.kt
@@ -6,19 +6,34 @@ import android.util.Base64
 import android.util.Log
 import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
+import com.theoplayer.BuildConfig
 import com.theoplayer.android.api.THEOplayerGlobal
 import com.theoplayer.android.api.contentprotection.*
+import com.theoplayer.android.api.error.ErrorCode
+import com.theoplayer.android.api.error.THEOplayerException
 import com.theoplayer.android.api.source.drm.DRMConfiguration
 import com.theoplayer.drm.ContentProtectionAdapter.fromDRMConfiguration
 
 const val TAG = "ContentProtectionModule"
 
-class ContentProtectionModule(private val context: ReactApplicationContext): ReactContextBaseJavaModule(context) {
+data class BridgeRequest(
+  val onResult: (result: ReadableMap) -> Unit,
+  val onError: (exception: THEOplayerException) -> Unit,
+  val onTimeout: Runnable
+)
+
+class ContentProtectionModule(private val context: ReactApplicationContext) :
+  ReactContextBaseJavaModule(context) {
+
+  companion object {
+    const val REQUEST_TIMEOUT_MS = 10000L
+  }
+
   private val handler = Handler(Looper.getMainLooper())
 
   private var requestId: Int = 0
 
-  private val requestQueue: HashMap<String, (result: ReadableMap) -> Unit> = HashMap()
+  private val requestQueue: HashMap<String, BridgeRequest> = HashMap()
 
   override fun getName(): String {
     return "ContentProtectionModule"
@@ -68,64 +83,67 @@ class ContentProtectionModule(private val context: ReactApplicationContext): Rea
     if (config != null) {
       payload.putMap(PROP_DRM_CONFIG, fromDRMConfiguration(config))
     }
-    val requestId = createRequestId()
-    payload.putString(PROP_REQUEST_ID, requestId)
-    requestQueue[requestId] = { result ->
-      // ignore
-    }
-    emit("onBuildIntegration", payload)
+    emit("onBuildIntegration", payload, {}, {})
   }
 
-  fun onCertificateRequest(integrationId: String, keySystemId: KeySystemId, request: Request, callback: CertificateRequestCallback) {
+  fun onCertificateRequest(
+    integrationId: String,
+    keySystemId: KeySystemId,
+    request: Request,
+    callback: CertificateRequestCallback
+  ) {
     val payload = ContentProtectionAdapter.fromRequest(integrationId, keySystemId, request)
-    val requestId = createRequestId()
-    payload.putString(PROP_REQUEST_ID, requestId)
-    requestQueue[requestId] = { result ->
+    emit("onCertificateRequest", payload, { result ->
       // Override properties from modified request
       request.url = result.getString(PROP_URL)!!
       request.method = RequestMethodAdapter.fromString(result.getString(PROP_METHOD))
       request.headers = result.getMap(PROP_HEADERS)?.toHashMap() as HashMap<String, String>
       request.body = Base64.decode(result.getString(PROP_BASE64_BODY), Base64.DEFAULT)
       callback.request(request)
-    }
-    emit("onCertificateRequest", payload)
+    }, { error -> callback.error(error) })
   }
 
-  fun onCertificateResponse(integrationId: String, keySystemId: KeySystemId, response: Response, callback: CertificateResponseCallback) {
+  fun onCertificateResponse(
+    integrationId: String,
+    keySystemId: KeySystemId,
+    response: Response,
+    callback: CertificateResponseCallback
+  ) {
     val payload = ContentProtectionAdapter.fromResponse(integrationId, keySystemId, response)
-    val requestId = createRequestId()
-    payload.putString(PROP_REQUEST_ID, requestId)
-    requestQueue[requestId] = { result ->
+    emit("onCertificateResponse", payload, { result ->
       val body = Base64.decode(result.getString(PROP_BASE64_BODY), Base64.DEFAULT)
       callback.respond(body)
-    }
-    emit("onCertificateResponse", payload)
+    }, { error -> callback.error(error) })
   }
 
-  fun onLicenseRequest(integrationId: String, keySystemId: KeySystemId, request: Request, callback: LicenseRequestCallback) {
+  fun onLicenseRequest(
+    integrationId: String,
+    keySystemId: KeySystemId,
+    request: Request,
+    callback: LicenseRequestCallback
+  ) {
     val payload = ContentProtectionAdapter.fromRequest(integrationId, keySystemId, request)
-    val requestId = createRequestId()
-    payload.putString(PROP_REQUEST_ID, requestId)
-    requestQueue[requestId] = { result ->
+    emit("onLicenseRequest", payload, { result ->
       // Override properties from modified request
       request.url = result.getString(PROP_URL)!!
       request.method = RequestMethodAdapter.fromString(result.getString(PROP_METHOD))
       request.headers = result.getMap(PROP_HEADERS)?.toHashMap() as HashMap<String, String>
       request.body = Base64.decode(result.getString(PROP_BASE64_BODY), Base64.DEFAULT)
       callback.request(request)
-    }
-    emit("onLicenseRequest", payload)
+    }, { error -> callback.error(error) })
   }
 
-  fun onLicenseResponse(integrationId: String, keySystemId: KeySystemId, response: Response, callback: LicenseResponseCallback) {
+  fun onLicenseResponse(
+    integrationId: String,
+    keySystemId: KeySystemId,
+    response: Response,
+    callback: LicenseResponseCallback
+  ) {
     val payload = ContentProtectionAdapter.fromResponse(integrationId, keySystemId, response)
-    val requestId = createRequestId()
-    payload.putString(PROP_REQUEST_ID, requestId)
-    requestQueue[requestId] = { result ->
+    emit("onLicenseResponse", payload, { result ->
       val body = Base64.decode(result.getString(PROP_BASE64_BODY), Base64.DEFAULT)
       callback.respond(body)
-    }
-    emit("onLicenseResponse", payload)
+    }, { error -> callback.error(error) })
   }
 
   @ReactMethod
@@ -136,21 +154,38 @@ class ContentProtectionModule(private val context: ReactApplicationContext): Rea
   fun removeListeners(count: Int?) {
   }
 
-  private fun emit(eventName: String, data: WritableMap) {
-    Log.d(TAG, "emit $eventName")
+  private fun emit(
+    eventName: String,
+    payload: WritableMap,
+    onSuccess: (result: ReadableMap) -> Unit,
+    onError: (error: THEOplayerException) -> Unit
+  ) {
+    val requestId = createRequestId()
+    if (BuildConfig.DEBUG) {
+      Log.d(TAG, "emit $eventName ($requestId)")
+    }
+    payload.putString(PROP_REQUEST_ID, requestId)
+    val onTimeout = {
+      onError(
+        THEOplayerException(ErrorCode.CONTENT_PROTECTION_ERROR, "$eventName time-out.")
+      )
+    }
+    requestQueue[requestId] = BridgeRequest(onSuccess, onError, onTimeout)
+    handler.postDelayed(onTimeout, REQUEST_TIMEOUT_MS)
     context
       .getJSModule(RCTDeviceEventEmitter::class.java)
-      .emit(eventName, data)
+      .emit(eventName, payload)
   }
 
   private fun receive(eventName: String, payload: ReadableMap) {
     val requestId = payload.getString(PROP_REQUEST_ID)
-    Log.d(TAG, "receive $eventName $requestId")
-    val response = requestQueue.remove(requestId)
-    if (response != null) {
-      response(payload)
-    } else {
-      Log.e(TAG, "receive $eventName failed");
+    if (BuildConfig.DEBUG) {
+      Log.d(TAG, "receive $eventName ($requestId)")
+    }
+    val request = requestQueue.remove(requestId)
+    if (request != null) {
+      handler.removeCallbacks(request.onTimeout)
+      request.onResult(payload)
     }
   }
 
