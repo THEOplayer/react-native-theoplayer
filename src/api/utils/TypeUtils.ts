659b3b88833fc09ecd57e30781d4812f627823f9
William Van Haevre <william.vanhaevre@gmail.com>
2022-11-22 13:13:12 +0100

    Update TypeUtils

--- src/api/utils/TypeUtils.ts
+++ src/api/utils/TypeUtils.ts
@@ -1,11 +1,14 @@
 import { Buffer } from 'buffer';
 
 // from object
+export function fromObjectToString(obj: { [key: string]: any }): string {
+  return JSON.stringify(obj);
+}
 export function fromObjectToBase64String(obj: { [key: string]: any }): string {
-  return fromStringToBase64String(JSON.stringify(obj));
+  return fromStringToBase64String(fromObjectToString(obj));
 }
 export function fromObjectToUint8Array(obj: { [key: string]: any }): Uint8Array {
-  return fromStringToUint8Array(JSON.stringify(obj));
+  return fromStringToUint8Array(fromObjectToString(obj));
 }
 
 // from string
@@ -14,6 +17,9 @@ export function fromStringToBase64String(str: string): string {
 }
 export function fromStringToUint8Array(str: string): Uint8Array {
   return Buffer.from(str, 'utf8');
+}
+export function fromStringToObject(str: string): { [key: string]: any } {
+  return JSON.parse(str);
 }
 
 // from base64 string
@@ -24,4 +30,10 @@ export function fromBase64StringToUint8Array(str: string): Uint8Array {
 // from uint8Array
 export function fromUint8ArrayToBase64String(array: Uint8Array): string {
   return Buffer.from(array).toString('base64');
+}
+export function fromUint8ArrayToString(array: Uint8Array): string {
+  return Buffer.from(array).toString('utf8');
+}
+export function fromUint8ArrayToObject(array: Uint8Array): { [key: string]: any } {
+  return fromStringToObject(fromUint8ArrayToString(array));
 }
