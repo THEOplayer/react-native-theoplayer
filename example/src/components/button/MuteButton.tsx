31276c35b6ceb3ee429958b744f2cb57a2c20df7
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-01-03 11:42:27 +0100

    Remove console logs

--- example/src/components/button/MuteButton.tsx
+++ example/src/components/button/MuteButton.tsx
@@ -11,7 +11,6 @@ export const MuteButton = () => {
   const toggleMuted = () => {
     const newMuted = !player.muted;
     player.muted = newMuted;
-    console.log('newMuted', newMuted);
     setMuted(newMuted);
   };
 
