c9e6775befcb036863eb5b733363437082ffeba3
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-01-03 15:05:02 +0100

    Don't show TopControlBar on TV's

--- example/src/components/controlbar/TopControlBar.tsx
+++ example/src/components/controlbar/TopControlBar.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import styles from '../videoplayer/VideoPlayerUI.style';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 
 export const TopControlBar = (props: React.PropsWithChildren<unknown>) => {
+  if (Platform.isTV) {
+    return <></>;
+  }
   return <View style={styles.topContainer}>{props.children}</View>;
 };
