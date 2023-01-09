a80fe255c3ce62968f0e712b7c3bcda23a6bfd7b
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-01-09 13:40:34 +0100

    Add fade effects for mobile

--- example/src/components/slots/SlotView.tsx
+++ example/src/components/slots/SlotView.tsx
@@ -1,6 +1,8 @@
 import React, { PureComponent, ReactNode } from 'react';
-import { Animated, StyleProp, StyleSheet, View, ViewStyle } from 'react-native';
+import { Animated, Platform, StyleProp, StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native';
 import { PlayerContext } from '../util/Context';
+import type { THEOplayerInternal } from 'react-native-theoplayer';
+import { PlayerEventType } from 'react-native-theoplayer';
 
 interface SlotViewProps {
   style?: StyleProp<ViewStyle>;
@@ -39,22 +41,76 @@ interface SlotViewState {
 }
 
 export class SlotView extends PureComponent<React.PropsWithChildren<SlotViewProps>, SlotViewState> {
+  private currentTimeout: number | undefined = undefined;
+
   constructor(props: SlotViewProps) {
     super(props);
+    this.state = {
+      fadeAnim: new Animated.Value(1),
+    };
   }
+
+  componentDidMount() {
+    const player = this.context as THEOplayerInternal;
+    player.addEventListener(PlayerEventType.PLAY, this.resetFadeOutTimeout);
+  }
+
+  componentWillUnmount() {
+    const player = this.context as THEOplayerInternal;
+    player.addEventListener(PlayerEventType.PLAY, this.resetFadeOutTimeout);
+  }
+
+  private fadeIn = () => {
+    if (Platform.isTV) {
+      // TODO fade effects for TV UI.
+      return;
+    }
+    this.resetFadeOutTimeout();
+    const { fadeAnim } = this.state;
+    Animated.timing(fadeAnim, {
+      useNativeDriver: true,
+      toValue: 1,
+      duration: 200,
+    }).start();
+  };
+
+  private resetFadeOutTimeout = () => {
+    if (Platform.isTV) {
+      // TODO fade effects for TV UI.
+      return;
+    }
+    clearTimeout(this.currentTimeout);
+    //@ts-ignore
+    this.currentTimeout = setTimeout(this.fadeOut, 2000);
+  };
+
+  private fadeOut = () => {
+    if (this.context.paused) {
+      // Player is paused, don't hide the UI.
+      return;
+    }
+    const { fadeAnim } = this.state;
+    Animated.timing(fadeAnim, {
+      useNativeDriver: true,
+      toValue: 0,
+      duration: 1000,
+    }).start();
+  };
 
   render() {
     const { style, top, center, bottom, children } = this.props;
     return (
       <>
-        <View style={[slotViewStyle.container]}>
-          <View style={[slotViewStyle.container, style]}>
+        <TouchableOpacity style={slotViewStyle.container} onPress={this.fadeIn} activeOpacity={0}></TouchableOpacity>
+        {/* The Animated.View is for showing and hiding the UI*/}
+        <Animated.View style={[slotViewStyle.container, { opacity: this.state.fadeAnim }]}>
+          <View style={[slotViewStyle.container, style]} onTouchStart={this.fadeIn}>
             <View style={slotViewStyle.topSlot}>{top}</View>
             <View style={slotViewStyle.centerSlot}>{center}</View>
             <View style={slotViewStyle.bottomSlot}>{bottom}</View>
             {children}
           </View>
-        </View>
+        </Animated.View>
       </>
     );
   }
