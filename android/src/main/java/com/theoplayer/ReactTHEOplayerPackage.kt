c48bf2e5508040593bdeadbc2cce3d0344a7c631
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2023-01-12 12:25:04 +0100

    Update ReactTHEOplayerView

---
+++ android/src/main/java/com/theoplayer/ReactTHEOplayerPackage.kt
@@ -0,0 +1,23 @@
+package com.theoplayer
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.uimanager.ViewManager
+import com.theoplayer.ads.AdsModule
+import com.theoplayer.drm.ContentProtectionModule
+import com.theoplayer.cast.CastModule
+
+class ReactTHEOplayerPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+      return listOf(
+        AdsModule(reactContext),
+        ContentProtectionModule(reactContext),
+        CastModule(reactContext)
+      )
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return listOf<ViewManager<*, *>>(ReactTHEOplayerViewManager())
+    }
+}
