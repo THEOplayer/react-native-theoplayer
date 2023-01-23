eeb3e4cd85974e073f15dbd54c1787ca00d92dfd
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2023-01-23 12:40:32 +0100

    Update viewResolver

--- android/src/main/java/com/theoplayer/util/ViewResolver.kt
+++ android/src/main/java/com/theoplayer/util/ViewResolver.kt
@@ -1,19 +1,18 @@
 package com.theoplayer.util
 
-import android.os.Handler
-import android.os.Looper
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.UIManagerModule
 import com.theoplayer.ReactTHEOplayerView
 
 class ViewResolver(private val reactContext: ReactApplicationContext) {
   private var uiManager: UIManagerModule? = null
-  private val handler = Handler(Looper.getMainLooper())
 
   fun resolveViewByTag(tag: Int, onResolved: (view: ReactTHEOplayerView?) -> Unit) {
     if (uiManager == null) {
       uiManager = reactContext.getNativeModule(UIManagerModule::class.java)
     }
-    handler.post { onResolved(uiManager?.resolveView(tag) as ReactTHEOplayerView) }
+    uiManager?.addUIBlock {
+      onResolved(it.resolveView(tag) as ReactTHEOplayerView)
+    }
   }
 }
