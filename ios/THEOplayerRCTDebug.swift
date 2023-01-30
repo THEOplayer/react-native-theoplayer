5979c48d759eb59b7de48a99e0bc06257553d76c
William Van Haevre <william.vanhaevre@gmail.com>
2023-01-30 16:11:46 +0100

    Drop state matching in RCTView and move player actions to playerModule

--- ios/THEOplayerRCTDebug.swift
+++ ios/THEOplayerRCTDebug.swift
@@ -16,7 +16,7 @@ let DEBUG_THEOPLAYER_INTERACTION = DEBUG && false
 let DEBUG_CONTENT_PROTECTION_API = DEBUG && false
 
 // Debug flag to monitor all updates made  on bridged properties
-let DEBUG_PROP_UPDATES = DEBUG && false
+let DEBUG_VIEW = DEBUG && false
 
 // Debug flag to monitor correct SourceDescription buildup
 let DEBUG_SOURCE_DESCRIPTION_BUIDER = DEBUG && false
