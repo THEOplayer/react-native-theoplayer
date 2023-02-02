e53b3490e9c2e1d4fb6d669b9f1181c44fb36484
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-02-02 11:42:19 +0100

    Fix activity indicator after rebase

--- example/src/components/delayedactivityindicator/CenteredDelayedActivityIndicator.tsx
+++ example/src/components/delayedactivityindicator/CenteredDelayedActivityIndicator.tsx
@@ -2,16 +2,18 @@ import React, { PureComponent } from 'react';
 import type { DelayedActivityIndicatorProps } from './DelayedActivityIndicator';
 import { DelayedActivityIndicator } from './DelayedActivityIndicator';
 import { View } from 'react-native';
-import { BufferingChangeEvent, PlayerEventType, THEOplayer } from 'react-native-theoplayer';
+import { ErrorEvent, PlayerError, PlayerEventType, ReadyStateChangeEvent, THEOplayer } from 'react-native-theoplayer';
 import { PlayerContext, PlayerWithStyle } from '../util/PlayerContext';
 
 interface CenteredDelayedActivityIndicatorState {
-  showing: boolean;
+  showLoadingIndicator: boolean;
+  error: PlayerError | undefined;
 }
 
 export class CenteredDelayedActivityIndicator extends PureComponent<DelayedActivityIndicatorProps, CenteredDelayedActivityIndicatorState> {
   private static initialState: CenteredDelayedActivityIndicatorState = {
-    showing: false,
+    showLoadingIndicator: false,
+    error: undefined,
   };
 
   constructor(props: DelayedActivityIndicatorProps) {
@@ -21,24 +23,60 @@ export class CenteredDelayedActivityIndicator extends PureComponent<DelayedActiv
 
   componentDidMount() {
     const player = this.context.player as THEOplayer;
-    player.addEventListener(PlayerEventType.BUFFERING_CHANGE, this.onBufferingStateChange);
+    player.addEventListener(PlayerEventType.READYSTATE_CHANGE, this.onReadyStateChange);
+    player.addEventListener(PlayerEventType.ERROR, this.onError);
+    player.addEventListener(PlayerEventType.PLAYING, this.onPlaying);
+    player.addEventListener(PlayerEventType.SOURCE_CHANGE, this.onSourceChange);
+    player.addEventListener(PlayerEventType.LOAD_START, this.onLoadStart);
   }
 
   componentWillUnmount() {
     const player = this.context.player as THEOplayer;
-    player.removeEventListener(PlayerEventType.BUFFERING_CHANGE, this.onBufferingStateChange);
+    player.removeEventListener(PlayerEventType.READYSTATE_CHANGE, this.onReadyStateChange);
+    player.removeEventListener(PlayerEventType.ERROR, this.onError);
+    player.removeEventListener(PlayerEventType.PLAYING, this.onPlaying);
+    player.removeEventListener(PlayerEventType.SOURCE_CHANGE, this.onSourceChange);
+    player.removeEventListener(PlayerEventType.LOAD_START, this.onLoadStart);
   }
 
-  private onBufferingStateChange = (event: BufferingChangeEvent) => {
-    this.setState({ showing: event.isBuffering });
+  private onReadyStateChange = (event: ReadyStateChangeEvent) => {
+    console.log(event);
+    this.maybeShowLoadingIndicator(event.readyState < 3);
   };
+
+  private onError = (event: ErrorEvent) => {
+    const { error } = event;
+    this.setState({ error });
+    this.maybeShowLoadingIndicator(false);
+  };
+
+  private onPlaying = () => {
+    this.maybeShowLoadingIndicator(false);
+  };
+
+  private onSourceChange = () => {
+    this.setState({ error: undefined });
+    this.maybeShowLoadingIndicator(false);
+  };
+
+  private onLoadStart = () => {
+    this.setState({ error: undefined });
+    this.maybeShowLoadingIndicator(true);
+  };
+
+  private maybeShowLoadingIndicator(showLoading: boolean) {
+    const { error } = this.state;
+    // do not change state to buffering in case of an error or if the player is paused
+    const player = this.context.player as THEOplayer;
+    this.setState({ showLoadingIndicator: showLoading && !error && !player.paused });
+  }
 
   render() {
     const { delay } = this.props;
-    const { showing } = this.state;
+    const { showLoadingIndicator } = this.state;
     const player = this.context.player as THEOplayer;
     return (
-      showing &&
+      showLoadingIndicator &&
       !player.paused && (
         <PlayerContext.Consumer>
           {(context: PlayerWithStyle) => (
