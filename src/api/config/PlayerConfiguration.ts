11dcf66e41207d578a76dfc7acd27e9bf35bdff9
Jeroen Veltmans <jeroen.veltmans@theoplayer.com>
2023-02-13 11:21:30 +0100

    Add mutedAutoplay to playerConfiguration

--- src/api/config/PlayerConfiguration.ts
+++ src/api/config/PlayerConfiguration.ts
@@ -15,6 +15,16 @@ export interface PlayerConfiguration {
   libraryLocation?: string;
 
   /**
+   * The muted autoplay policy for web.
+   *
+   * @remarks
+   * <br/> - The muted autoplay policy is impacted by this property and {@link SourceConfiguration.mutedAutoplay}.
+   *
+   * @defaultValue `'none'`.
+   */
+  mutedAutoplay?: MutedAutoplayConfiguration;
+
+  /**
    * The ads configuration for the player.
    */
   ads?: AdsConfiguration;
@@ -39,3 +49,13 @@ export interface PlayerConfiguration {
    */
   readonly chromeless?: boolean;
 }
+
+/**
+ * The muted autoplay policy of a player for web.
+ * <br/> - `'none'`: Disallow muted autoplay. If the player is requested to autoplay while unmuted, and the platform does not support unmuted autoplay, the player will not start playback.
+ * <br/> - `'all'`: Allow muted autoplay. If the player is requested to autoplay while unmuted, and the platform supports muted autoplay, the player will start muted playback.
+ * <br/> - `'content'`: Allow muted autoplay only for the main content. Disallow muted autoplay for e.g. advertisements. (Not yet supported.)
+ *
+ * @public
+ */
+export type MutedAutoplayConfiguration = 'none' | 'all' | 'content';
