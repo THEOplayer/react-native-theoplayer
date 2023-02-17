4b3dfd3c8fe3d0bf3a808ddaabb44074fbe8d89e
William Van Haevre <william.vanhaevre@gmail.com>
2023-02-17 13:06:48 +0100

    Add iOS PipConfiguration setup

---
+++ ios/pip/THEOplayerRCTView+PipConfig.swift
@@ -0,0 +1,45 @@
+// THEOplayerRCTView+Ads.swift
+
+import Foundation
+import THEOplayerSDK
+
+struct PipConfig {
+    var retainPresentationModeOnSourceChange: Bool = false
+    var canStartPictureInPictureAutomaticallyFromInline: Bool = false
+}
+
+#if os(iOS)
+
+extension THEOplayerRCTView {
+    
+    func parsePipConfig(configDict: NSDictionary) {
+        if let pipConfig = configDict["pip"] as? NSDictionary {
+            self.pipConfig.retainPresentationModeOnSourceChange = pipConfig["retainPresentationModeOnSourceChange"] as? Bool ?? false
+            self.pipConfig.canStartPictureInPictureAutomaticallyFromInline = pipConfig["canStartPictureInPictureAutomaticallyFromInline"] as? Bool ?? false
+        }
+    }
+
+    func playerPipConfiguration() -> PiPConfiguration? {
+        return PiPConfiguration(retainPresentationModeOnSourceChange: self.pipConfig.retainPresentationModeOnSourceChange,
+                                nativePictureInPicture: true,
+                                canStartPictureInPictureAutomaticallyFromInline: self.pipConfig.canStartPictureInPictureAutomaticallyFromInline)
+    }
+}
+
+#elseif os(tvOS)
+
+extension THEOplayerRCTView {
+    
+    func parsePipConfig(configDict: NSDictionary) {
+        if let pipConfig = configDict["pip"] as? NSDictionary {
+            self.pipConfig.retainPresentationModeOnSourceChange = pipConfig["retainPresentationModeOnSourceChange"] as? Bool ?? false
+        }
+    }
+    
+    func playerPipConfiguration() -> PiPConfiguration? {
+        return PiPConfiguration(retainPresentationModeOnSourceChange: self.pipConfig.retainPresentationModeOnSourceChange)
+    }
+    
+}
+
+#endif
