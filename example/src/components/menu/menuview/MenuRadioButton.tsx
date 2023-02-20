8daa557e6d058812c1ab3bc77908fb48913acd37
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-02-20 17:29:51 +0100

    Add new framework for showing menu in UI

---
+++ example/src/components/menu/menuview/MenuRadioButton.tsx
@@ -0,0 +1,32 @@
+import { Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { PlayerContext, UiContext } from '../../util/PlayerContext';
+
+export interface MenuRowProps {
+  label: string;
+  id: number;
+  selected?: boolean;
+  onSelect: (id: number) => void;
+}
+
+export const MenuRadioButton = (props: MenuRowProps) => {
+  const { id, onSelect, label, selected } = props;
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        onSelect(id);
+      }}>
+      <PlayerContext.Consumer>
+        {(context: UiContext) => (
+          <Text
+            style={[
+              context.style.MenuView.row,
+              selected ? { color: context.style.colors.textBackground, backgroundColor: context.style.colors.text } : {},
+            ]}>
+            {label}
+          </Text>
+        )}
+      </PlayerContext.Consumer>
+    </TouchableOpacity>
+  );
+};
