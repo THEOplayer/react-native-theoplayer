0667b96ebefc39ce0dfe38ffe89a0529c49dc842
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2023-02-21 12:11:35 +0100

    Update pip configuration

--- src/api/pip/PiPConfiguration.ts
+++ src/api/pip/PiPConfiguration.ts
@@ -4,27 +4,18 @@
  * @public
  */
 export interface PiPConfiguration {
-    /**
-     * The maximum percentage of the original player position that should be visible to enable picture-in-picture automatically.
-     *
-     * @remarks
-     * <br/> - If not configured, picture-in-picture can only be activated by calling {@link Presentation.requestMode} with the `'picture-in-picture'` argument.
-     *
-     * @defaultValue `undefined`
-     */
-    // visibility?: number | undefined;
+  /**
+   * Whether the presentation mode should be retained on source changes.
+   *
+   * @defaultValue `false`
+   */
+  retainPresentationModeOnSourceChange?: boolean;
 
-    /**
-     * Whether the presentation mode should be retained on source changes.
-     *
-     * @defaultValue `false`
-     */
-    retainPresentationModeOnSourceChange?: boolean;
-
-    /**
-     * Whether Picture in Picture should be allowed to start automatically when transitioning to background when the receiver’s content is embedded inline. Default is NO.
-     *
-     * @defaultValue `false`
-     */
-    canStartPictureInPictureAutomaticallyFromInline?: boolean;
+  /**
+   * Whether Picture in Picture should be allowed to start automatically when transitioning to background if the player's current presentation mode
+   * is `inline`.
+   *
+   * @defaultValue `false`
+   */
+  canStartPictureInPictureAutomaticallyFromInline?: boolean;
 }
