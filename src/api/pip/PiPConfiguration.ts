c8985026dbe7d45f0c7bf983f3ead4cdd08b4495
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2023-02-24 10:35:50 +0100

    Remove retainPresentationModeOnSourceChange pip config property

--- src/api/pip/PiPConfiguration.ts
+++ src/api/pip/PiPConfiguration.ts
@@ -5,13 +5,6 @@
  */
 export interface PiPConfiguration {
   /**
-   * Whether the presentation mode should be retained on source changes.
-   *
-   * @defaultValue `false`
-   */
-  retainPresentationModeOnSourceChange?: boolean;
-
-  /**
    * Whether Picture in Picture should be allowed to start automatically when transitioning to background if the player's current presentation mode
    * is `inline`.
    *
