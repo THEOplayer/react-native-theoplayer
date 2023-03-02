9f228bebcf9c10538e28187ad220ff7981b6dafc
William Van Haevre <william.vanhaevre@gmail.com>
2023-03-02 09:14:16 +0100

    Stop parsing pipConfig from RN PlayerConfig

--- ios/pip/THEOplayerRCTView+PipConfig.swift
+++ ios/pip/THEOplayerRCTView+PipConfig.swift
@@ -12,13 +12,6 @@ struct PipConfig {
 #if os(iOS)
 
 extension THEOplayerRCTView {
-    
-    func parsePipConfig(configDict: NSDictionary) {
-        if let pipConfig = configDict["pip"] as? NSDictionary {
-            self.pipConfig.retainPresentationModeOnSourceChange = pipConfig["retainPresentationModeOnSourceChange"] as? Bool ?? false
-            self.pipConfig.canStartPictureInPictureAutomaticallyFromInline = pipConfig["canStartPictureInPictureAutomaticallyFromInline"] as? Bool ?? false
-        }
-    }
 
     func playerPipConfiguration() -> PiPConfiguration? {
         return PiPConfiguration(retainPresentationModeOnSourceChange: self.pipConfig.retainPresentationModeOnSourceChange,
@@ -30,12 +23,6 @@ extension THEOplayerRCTView {
 #elseif os(tvOS)
 
 extension THEOplayerRCTView {
-    
-    func parsePipConfig(configDict: NSDictionary) {
-        if let pipConfig = configDict["pip"] as? NSDictionary {
-            self.pipConfig.retainPresentationModeOnSourceChange = pipConfig["retainPresentationModeOnSourceChange"] as? Bool ?? false
-        }
-    }
     
     func playerPipConfiguration() -> PiPConfiguration? {
         return PiPConfiguration(retainPresentationModeOnSourceChange: self.pipConfig.retainPresentationModeOnSourceChange)
