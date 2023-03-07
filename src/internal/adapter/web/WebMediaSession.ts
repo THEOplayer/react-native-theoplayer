73e53e6d8ccd993a077d1aee9f5d9d30fb9db7bf
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2023-03-07 10:41:27 +0100

    Fix infinite duration playbackState

--- src/internal/adapter/web/WebMediaSession.ts
+++ src/internal/adapter/web/WebMediaSession.ts
@@ -24,7 +24,7 @@ const mediaSession = (function () {
  * The MediaSession interface of the Media Session API allows a web page to provide custom behaviors for standard media playback interactions, and
  * to report metadata that can be sent by the user agent to the device or operating system for presentation in standardized user interface elements.
  *
- * @link https://developer.mozilla.org/en-US/docs/Web/API/MediaSession
+ * @link https://w3c.github.io/mediasession
  */
 export class WebMediaSession {
   private readonly _config: WebMediaSessionConfig;
@@ -36,7 +36,6 @@ export class WebMediaSession {
     this._player.addEventListener('sourcechange', this.onSourceChange);
     this._player.addEventListener('play', this.onPlay);
     this._player.addEventListener('pause', this.onPause);
-    this._player.addEventListener('timeupdate', this.updatePositionState);
     this._player.ads?.addEventListener('adbreakbegin', this.onAdbreakBegin);
     this._player.ads?.addEventListener('adbreakend', this.onAdbreakEnd);
 
@@ -71,7 +70,6 @@ export class WebMediaSession {
     this._player.removeEventListener('sourcechange', this.onSourceChange);
     this._player.removeEventListener('play', this.onPlay);
     this._player.removeEventListener('pause', this.onPause);
-    this._player.removeEventListener('timeupdate', this.updatePositionState);
     this._player.ads?.removeEventListener('adbreakbegin', this.onAdbreakBegin);
     this._player.ads?.removeEventListener('adbreakend', this.onAdbreakEnd);
     mediaSession.setActionHandler('play', null);
@@ -100,17 +98,22 @@ export class WebMediaSession {
 
   private updatePositionState = () => {
     const { duration, playbackRate, currentTime } = this._player;
-    mediaSession.setPositionState({
-      // A floating-point value giving the total duration of the current media in seconds. This should always be a positive number,
-      // with positive infinity (Infinity) indicating media without a defined end, such as a live stream.
-      duration: isNaN(duration) ? 0 : duration,
+    const isLive = !isFinite(duration);
+    if (!isLive) {
+      mediaSession.setPositionState({
+        // The duration is used to specify the duration in seconds.
+        // It should always be positive and positive infinity can be used to indicate media without a defined end such as live playback.
+        // NOTE: passing Infinite causes an exception to be thrown.
+        duration: isNaN(duration) || duration < 0 ? 0 : duration,
 
-      // A floating-point value indicating the rate at which the media is being played, as a ratio relative to its normal playback speed.
-      playbackRate,
+        // The playbackRate is used to specify the playback rate.
+        // It can be positive to represent forward playback or negative to represent backwards playback. It should not be zero.
+        playbackRate,
 
-      // A floating-point value indicating the last reported playback position of the media in seconds. This must always be a positive value.
-      position: currentTime,
-    });
+        // The position is used to specify the last reported playback position in seconds. It should always be positive.
+        position: currentTime,
+      });
+    }
   };
 
   private onPlay = () => {
