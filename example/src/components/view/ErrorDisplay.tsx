99f1539580d7ff7829c29a9bf2faa7dfaad5c4c0
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-03-07 11:56:28 +0100

    Show fullscreen error on top of UI

--- example/src/components/view/ErrorDisplay.tsx
+++ example/src/components/view/ErrorDisplay.tsx
@@ -1,70 +1,38 @@
-import React, { PureComponent } from 'react';
-import { CastEvent, CastEventType, ErrorEvent, PlayerError, PlayerEventType } from 'react-native-theoplayer';
+import React from 'react';
 import { Text, View } from 'react-native';
+import type { PlayerError } from 'react-native-theoplayer';
 import { PlayerContext, UiContext } from '../util/PlayerContext';
+import { SvgContext } from '../button/svg/SvgUtils';
+import { ErrorSvg } from '../button/svg/ErrorSvg';
 
-interface ErrorDisplayState {
-  error: PlayerError | undefined;
+interface ErrorDisplayProps {
+  error: PlayerError;
 }
 
-export class ErrorDisplay extends PureComponent<unknown, ErrorDisplayState> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = { error: undefined };
-  }
-
-  componentDidMount() {
-    const player = (this.context as UiContext).player;
-    player.addEventListener(PlayerEventType.LOAD_START, this.onLoadStart);
-    player.addEventListener(PlayerEventType.ERROR, this.onError);
-    player.addEventListener(PlayerEventType.CAST_EVENT, this.onCastEvent);
-  }
-
-  componentWillUnmount() {
-    const player = (this.context as UiContext).player;
-    player.removeEventListener(PlayerEventType.LOAD_START, this.onLoadStart);
-    player.removeEventListener(PlayerEventType.ERROR, this.onError);
-    player.removeEventListener(PlayerEventType.CAST_EVENT, this.onCastEvent);
-  }
-
-  private onLoadStart = () => {
-    this.setState({ error: undefined });
-  };
-
-  private onError = (event: ErrorEvent) => {
-    const { error } = event;
-    this.setState({ error });
-  };
-
-  private onCastEvent = (event: CastEvent) => {
-    if (event.subType === CastEventType.CHROMECAST_ERROR)
-      this.setState({
-        error: {
-          errorCode: event.error.errorCode,
-          errorMessage: event.error.description,
-        },
-      });
-  };
-
-  render() {
-    const { error } = this.state;
-
-    if (!error) {
-      return <></>;
-    }
-
-    return (
-      <PlayerContext.Consumer>
-        {(context: UiContext) => (
-          <View style={context.style.fullScreenCenter}>
-            <Text style={[context.style.text, { color: context.style.colors.text, backgroundColor: context.style.colors.secondary }]}>
-              {error.errorCode} - {error.errorMessage}
-            </Text>
+export function ErrorDisplay(props: ErrorDisplayProps) {
+  const { error } = props;
+  return (
+    <PlayerContext.Consumer>
+      {(context: UiContext) => (
+        <View style={[context.style.fullScreenCenter, { backgroundColor: context.style.colors.secondary }]}>
+          <View style={{ flexDirection: 'row' }}>
+            <SvgContext.Provider
+              value={{
+                fill: context.style.colors.primary,
+                height: '100%',
+                width: '100%',
+              }}>
+              <View style={[context.style.controlBar.buttonIcon]}>
+                <ErrorSvg />
+              </View>
+            </SvgContext.Provider>
+            <Text style={[context.style.text, { color: context.style.colors.text, alignSelf: 'center', paddingLeft: 5 }]}>Error:</Text>
           </View>
-        )}
-      </PlayerContext.Consumer>
-    );
-  }
+          <Text style={[context.style.text, { color: context.style.colors.text, backgroundColor: context.style.colors.secondary }]}>
+            {error.errorMessage}
+          </Text>
+        </View>
+      )}
+    </PlayerContext.Consumer>
+  );
 }
-
-ErrorDisplay.contextType = PlayerContext;
