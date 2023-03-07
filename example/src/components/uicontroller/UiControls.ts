d41ff81b556c967e3ea82425768a0abb1dcb5f2d
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-03-07 11:14:49 +0100

    Add docs

--- example/src/components/uicontroller/UiControls.ts
+++ example/src/components/uicontroller/UiControls.ts
@@ -3,15 +3,33 @@ import type { ReactNode } from 'react';
 export type MenuContructor = () => ReactNode;
 
 export interface UiControls {
+  /*
+   * Whether the UI buttons are enabled.
+   */
   readonly buttonsEnabled_: boolean;
 
+  /*
+   * Used to notify the UI that a user pressed did an action, so the UI can be faded in.
+   */
   onUserAction_: () => void;
 
+  /*
+   * Sets the user as "active" and does not allow the UI to be faded out.
+   */
   setUserActive_: () => number;
 
+  /*
+   * Sets the user as "idle" and allows the UI to fade out again.
+   */
   setUserIdle_: (id: number) => void;
 
+  /*
+   * Opens a new menu on top of the player.
+   */
   openMenu_: (menuConstructor: MenuContructor) => void;
 
+  /*
+   * Closes the current menu. If this was a sub-menu, the parent menu will re-open.
+   */
   closeCurrentMenu_: () => void;
 }
