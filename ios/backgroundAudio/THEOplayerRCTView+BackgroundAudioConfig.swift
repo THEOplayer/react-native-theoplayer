d73b2a817f15110a0f012b4f964a7cfcf8245822
William Van Haevre <william.vanhaevre@gmail.com>
2023-03-17 14:02:03 +0100

    Add BG Audio support for native iOS setup

--- ios/backgroundAudio/THEOplayerRCTView+BackgroundAudioConfig.swift
+++ ios/backgroundAudio/THEOplayerRCTView+BackgroundAudioConfig.swift
@@ -6,3 +6,17 @@ import THEOplayerSDK
 struct BackgroundAudioConfig {
     var enabled: Bool = false
 }
+
+extension THEOplayerRCTView: BackgroundPlaybackDelegate {
+
+    func initBackgroundAudio() {
+        self.player?.backgroundPlaybackDelegate = self
+    }
+    
+    public func shouldContinueAudioPlaybackInBackground() -> Bool {
+        // Make sure to go to the background with updated NowPlayingInfo
+        self.nowPlayingManager.updateNowPlaying()
+        
+        return self.backgroundAudioConfig.enabled
+    }
+}
