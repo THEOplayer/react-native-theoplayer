509c8476b9e28924e6c49d25a8ef60e62d8580dd
William Van Haevre <william.vanhaevre@gmail.com>
2023-03-17 13:54:59 +0100

    Remove BG audio support for non-native setup

--- ios/backgroundAudio/THEOplayerRCTView+BackgroundAudioConfig.swift
+++ ios/backgroundAudio/THEOplayerRCTView+BackgroundAudioConfig.swift
@@ -6,17 +6,3 @@ import THEOplayerSDK
 struct BackgroundAudioConfig {
     var enabled: Bool = false
 }
-
-extension THEOplayerRCTView: BackgroundPlaybackDelegate {
-
-    func initBackgroundAudio() {
-        self.player?.backgroundPlaybackDelegate = self
-    }
-    
-    public func shouldContinueAudioPlaybackInBackground() -> Bool {
-        // Make sure to go to the background with updated NowPlayingInfo
-        self.nowPlayingManager.updateNowPlaying()
-        
-        return self.backgroundAudioConfig.enabled
-    }
-}
