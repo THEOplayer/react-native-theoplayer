6b0e91bc746b0cea1e33279a14b64a97f600b6ff
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-03-20 16:13:32 +0100

    Pass submenus as child props instead

--- example/src/components/menu/SettingsMenuButton.tsx
+++ example/src/components/menu/SettingsMenuButton.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
-import { View } from 'react-native';
 import { MenuButton } from './common/MenuButton';
 import { SettingsSvg } from '../button/svg/SettingsSvg';
 import { MenuView } from './common/MenuView';
 import { ScrollableMenu } from './common/ScrollableMenu';
-import { QualitySubMenu } from './QualitySubMenu';
-import { PlaybackRateSubMenu } from './PlaybackRateSubMenu';
 
-export const SettingsMenuButton = () => {
+export const SettingsMenuButton = (props: React.PropsWithChildren<unknown>) => {
+  const { children } = props;
   const createMenu = () => {
-    return (
-      <MenuView
-        menu={
-          <ScrollableMenu
-            title={'Settings'}
-            items={
-              <View>
-                <QualitySubMenu />
-                <PlaybackRateSubMenu />
-              </View>
-            }
-          />
-        }
-      />
-    );
+    return <MenuView menu={<ScrollableMenu title={'Settings'} items={children} />} />;
   };
 
   return <MenuButton svg={<SettingsSvg />} menuConstructor={createMenu} />;
