759f02c1141d2b7044451d1c1e3762239fa999c9
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-03-20 16:23:05 +0100

    Add PipButton

---
+++ example/src/components/button/PipButton.tsx
@@ -0,0 +1,68 @@
+import React, { PureComponent } from 'react';
+import type { PresentationMode, PresentationModeChangeEvent } from 'react-native-theoplayer';
+import { PlayerEventType } from 'react-native-theoplayer';
+import { Platform } from 'react-native';
+import { ActionButton } from './actionbutton/ActionButton';
+import { PlayerContext, UiContext } from '../util/PlayerContext';
+import { PipExitSvg } from './svg/PipExitSvg';
+import { PipEnterSvg } from './svg/PipEnterSvg';
+
+interface PipButtonState {
+  presentationMode: PresentationMode;
+}
+
+export class PipButton extends PureComponent<unknown, PipButtonState> {
+  constructor(props: unknown) {
+    super(props);
+    this.state = { presentationMode: 'inline' };
+  }
+
+  componentDidMount() {
+    const player = (this.context as UiContext).player;
+    player.addEventListener(PlayerEventType.PRESENTATIONMODE_CHANGE, this.onPresentationModeChange);
+    this.setState({ presentationMode: player.presentationMode });
+  }
+
+  componentWillUnmount() {
+    const player = (this.context as UiContext).player;
+    player.removeEventListener(PlayerEventType.PRESENTATIONMODE_CHANGE, this.onPresentationModeChange);
+  }
+
+  private readonly onPresentationModeChange = (event: PresentationModeChangeEvent) => {
+    this.setState({ presentationMode: event.presentationMode });
+  };
+
+  private togglePip = () => {
+    const player = (this.context as UiContext).player;
+    switch (player.presentationMode) {
+      case 'inline':
+      case 'fullscreen':
+        player.presentationMode = 'picture-in-picture';
+        break;
+      case 'picture-in-picture':
+        player.presentationMode = 'inline';
+        break;
+    }
+  };
+
+  render() {
+    if (Platform.isTV) {
+      return <></>;
+    }
+    const { presentationMode } = this.state;
+
+    return (
+      <PlayerContext.Consumer>
+        {(context: UiContext) => (
+          <ActionButton
+            svg={presentationMode === 'picture-in-picture' ? <PipExitSvg /> : <PipEnterSvg />}
+            onPress={this.togglePip}
+            iconStyle={context.style.controlBar.buttonIcon}
+          />
+        )}
+      </PlayerContext.Consumer>
+    );
+  }
+}
+
+PipButton.contextType = PlayerContext;
