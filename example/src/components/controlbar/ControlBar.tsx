d36b9dd2bc53043bf80d816b48dcecb106b66152
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-03-21 13:30:57 +0100

    Add CenteredControlBar

--- example/src/components/controlbar/ControlBar.tsx
+++ example/src/components/controlbar/ControlBar.tsx
@@ -1,6 +1,8 @@
 import React, { ReactNode } from 'react';
 import { StyleProp, View, ViewStyle } from 'react-native';
 import { PlayerContext, UiContext } from '../util/PlayerContext';
+import { CENTER_BUTTON_SIZE } from '../THEOplayerStyle';
+import { Spacer } from './Spacer';
 
 export type SlotType = 'top' | 'bottom' | 'center';
 
@@ -17,5 +19,27 @@ export const ControlBar = (props: React.PropsWithChildren<ControlBarProps>) => {
     <PlayerContext.Consumer>
       {(context: UiContext) => <View style={[context.style.controlBar.container, style]}>{children}</View>}
     </PlayerContext.Consumer>
+  );
+};
+
+interface CenteredControlBarProps {
+  style?: StyleProp<ViewStyle>;
+  left?: ReactNode;
+  middle?: ReactNode;
+  right?: ReactNode;
+}
+
+export const CenteredControlBar = (props: CenteredControlBarProps) => {
+  const { style, middle, left, right } = props;
+  return (
+    <ControlBar style={[{ height: CENTER_BUTTON_SIZE }, style]}>
+      <Spacer grow={3} />
+      {left}
+      <Spacer />
+      {middle}
+      <Spacer />
+      {right}
+      <Spacer grow={3} />
+    </ControlBar>
   );
 };
