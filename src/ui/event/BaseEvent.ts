976b242726e484621ddb91ce295f9539c42661f9
George Choustoulakis <george.choustoulakis@theoplayer.com>
2023-04-02 18:13:34 +0200

    Remove duplicate event dispatcher

--- src/ui/event/BaseEvent.ts
+++
@@ -1,5 +0,0 @@
-import type { Event } from './Event';
-
-export class BaseEvent<TType extends string = string> implements Event<TType> {
-  constructor(readonly type: TType, readonly date: Date = new Date()) {}
-}
--- src/ui/event/EventListener.ts
+++
@@ -1,8 +0,0 @@
-import type { Event } from './Event';
-
-/**
- * The function to be executed when an event occurred.
- *
- * @public
- */
-export type EventListener<TEvent extends Event> = (event: TEvent) => void;
