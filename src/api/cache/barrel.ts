c0674e7a947d54ab9fdaf9d705dbab8a137dbb22
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-07-05 17:41:45 +0200

    Add cache react hooks

---
+++ src/api/cache/barrel.ts
@@ -0,0 +1,7 @@
+export * from './CachingTask';
+export * from './CachingTaskLicense';
+export * from './CachingTaskList';
+export * from './CachingTaskParameters';
+export * from './MediaCacheAPI';
+export * from './events/barrel';
+export * from './hooks/barrel';
---
+++ src/api/cache/events/barrel.ts
@@ -0,0 +1,5 @@
+export * from './AddCachingTaskEvent';
+export * from './RemoveCachingTaskEvent';
+export * from './CacheEvent';
+export * from './CachingTaskEvent';
+export * from './CachingTaskListEvent';
