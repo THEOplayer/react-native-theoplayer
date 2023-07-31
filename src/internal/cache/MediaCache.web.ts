f149c9d453cc0fec3447271b9e1d9ccff18c201e
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-07-31 09:50:52 +0200

    Update web cache

--- src/internal/cache/MediaCache.web.ts
+++ src/internal/cache/MediaCache.web.ts
@@ -6,10 +6,35 @@ import type { SourceDescription } from 'react-native-theoplayer';
 import type { CachingTaskParameters } from 'react-native-theoplayer';
 import type { CachingTask } from 'react-native-theoplayer';
 import * as THEOplayerWeb from 'theoplayer';
+import { WebCachingTaskAdapter } from './WebCachingTaskAdapter';
+import type { AddCachingTaskEvent, RemoveCachingTaskEvent } from 'theoplayer';
+import { CacheEventType } from 'react-native-theoplayer';
 
 export class WebMediaCache extends DefaultEventDispatcher<CacheEventMap> implements MediaCacheAPI {
+  constructor() {
+    super();
+    THEOplayerWeb.cache.tasks.addEventListener('addtask', this.onAddTask);
+    THEOplayerWeb.cache.tasks.addEventListener('removetask', this.onRemoveTask);
+  }
+
+  private onAddTask = (event: AddCachingTaskEvent) => {
+    this.dispatchEvent({
+      type: CacheEventType.addtask,
+      task: new WebCachingTaskAdapter(event.task),
+      date: event.date,
+    });
+  };
+
+  private onRemoveTask = (event: RemoveCachingTaskEvent) => {
+    this.dispatchEvent({
+      type: CacheEventType.removetask,
+      task: new WebCachingTaskAdapter(event.task),
+      date: event.date,
+    });
+  };
+
   createTask(source: SourceDescription, parameters: CachingTaskParameters): Promise<CachingTask> {
-    return Promise.resolve(THEOplayerWeb.cache.createTask(source, parameters) as CachingTask);
+    return Promise.resolve(new WebCachingTaskAdapter(THEOplayerWeb.cache.createTask(source, parameters)));
   }
 
   get status(): CacheStatus {
@@ -18,7 +43,7 @@ export class WebMediaCache extends DefaultEventDispatcher<CacheEventMap> impleme
 
   // @ts-ignore
   get tasks(): CachingTaskList {
-    return THEOplayerWeb.cache.tasks as CachingTaskList;
+    return THEOplayerWeb.cache.tasks.map((task) => new WebCachingTaskAdapter(task)) as CachingTaskList;
   }
 }
 
