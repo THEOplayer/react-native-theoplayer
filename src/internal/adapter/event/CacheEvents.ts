8019232c988f991354e167b709b1e911f56e0177
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-08-01 12:30:25 +0200

    Remove unused file

--- src/internal/adapter/event/CacheEvents.ts
+++
@@ -1,7 +0,0 @@
-import { BaseEvent } from './BaseEvent';
-import {CacheEventType, CacheStatus, CacheStatusChangeEvent} from 'react-native-theoplayer';
-export class DefaultCacheStatusChangeEvent extends BaseEvent<CacheEventType.statechange> implements CacheStatusChangeEvent {
-  constructor(public status: CacheStatus) {
-    super(CacheEventType.statechange);
-  }
-}
