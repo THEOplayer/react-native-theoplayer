3d5c6441f82203667b117501d65fa4863504203c
William Van Haevre <william.vanhaevre@gmail.com>
2023-12-05 12:36:54 +0100

    Pass node when broadcasting an event

--- ios/eventBroadcasting/THEOplayerRCTEventBroadcastAPI.swift
+++ ios/eventBroadcasting/THEOplayerRCTEventBroadcastAPI.swift
@@ -26,12 +26,12 @@ class THEOplayerRCTBroadcastAPI: NSObject, RCTBridgeModule {
     func broadcastEvent(_ node: NSNumber, event: NSDictionary) -> Void {
         DispatchQueue.main.async {
             if self.bridge.uiManager.view(forReactTag: node) is THEOplayerRCTView {
-                let selector = NSSelectorFromString("onEventBroadcasted:")
+                let selector = NSSelectorFromString("onEventBroadcasted:event:")
                 self.bridge.moduleClasses.forEach { moduleClass in
                     if let nativeModule = self.bridge.module(for: moduleClass) as? RCTBridgeModule,
                        nativeModule.responds(to: selector) {
                         let nativeEvent = THEOplayerRCTBroadcastEventAdapter.toEvent(eventData: event)
-                        nativeModule.perform(selector, with: nativeEvent)
+                        nativeModule.perform(selector, with: node, with: nativeEvent)
                     }
                 }
             }
