a0bc14b34295ccfba69efb3528078cc178d4ebb5
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-12-06 21:04:49 +0100

    Move broadcast to player

--- android/src/main/java/com/theoplayer/broadcast/EventBroadcastModule.kt
+++ android/src/main/java/com/theoplayer/broadcast/EventBroadcastModule.kt
@@ -1,15 +1,13 @@
+@file:Suppress("unused")
 package com.theoplayer.broadcast
 
-import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.module.annotations.ReactModule
 import com.theoplayer.ReactTHEOplayerView
-import com.theoplayer.android.api.event.Event
 import com.theoplayer.util.ViewResolver
-import java.lang.Exception
 
 private const val TAG = "EventBroadcastModule"
 
@@ -26,29 +24,13 @@ class EventBroadcastModule(context: ReactApplicationContext) : ReactContextBaseJ
     return TAG
   }
 
+  /**
+   * Receive a react-native broadcast event and route it to the player's broadcast adapter.
+   */
   @ReactMethod
   fun broadcastEvent(tag: Int, event: ReadableMap) {
-    // Map target names to native modules.
-    val modules = reactApplicationContext.nativeModules
-
-    // No modules to forward to.
-    if (modules.isEmpty()) {
-      return
-    }
-
-    // Route the event to each module
-    EventAdapter.parseEvent(event)?.also {
-      modules.forEach { module ->
-        broadcastEvent(tag, module, it)
-      }
-    }
-  }
-
-  fun broadcastEvent(tag: Int, target: NativeModule, event: Event<*>) {
-    try {
-      (target as? EventBroadcastReceiver)?.onEventBroadcasted(tag, event)
-    } catch (e: Exception) {
-      // Module does not accept event.
+    viewResolver.resolveViewByTag(tag) { view: ReactTHEOplayerView? ->
+      view?.broadcast?.broadcastEvent(event)
     }
   }
 }
--- android/src/main/java/com/theoplayer/broadcast/EventBroadcastReceiver.kt
+++
@@ -1,7 +0,0 @@
-package com.theoplayer.broadcast
-
-import com.theoplayer.android.api.event.Event
-
-interface EventBroadcastReceiver {
-  fun onEventBroadcasted(tag: Int, event: Event<*>)
-}
