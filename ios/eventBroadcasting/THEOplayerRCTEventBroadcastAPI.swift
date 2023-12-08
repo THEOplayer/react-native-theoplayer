87b24fa24cadae72e4c7148df01ed31ec6fdccdd
William Van Haevre <william.vanhaevre@gmail.com>
2023-12-08 11:14:05 -0800

    Rework to internal broadcast event dispatcher for iOS

--- ios/eventBroadcasting/THEOplayerRCTEventBroadcastAPI.swift
+++ ios/eventBroadcasting/THEOplayerRCTEventBroadcastAPI.swift
@@ -25,15 +25,8 @@ class THEOplayerRCTBroadcastAPI: NSObject, RCTBridgeModule {
     @objc(broadcastEvent:event:)
     func broadcastEvent(_ node: NSNumber, event: NSDictionary) -> Void {
         DispatchQueue.main.async {
-            if self.bridge.uiManager.view(forReactTag: node) is THEOplayerRCTView {
-                let selector = NSSelectorFromString("onEventBroadcasted:event:")
-                self.bridge.moduleClasses.forEach { moduleClass in
-                    if let nativeModule = self.bridge.module(for: moduleClass) as? RCTBridgeModule,
-                       nativeModule.responds(to: selector) {
-                        let nativeEvent = THEOplayerRCTBroadcastEventAdapter.toEvent(eventData: event)
-                        nativeModule.perform(selector, with: node, with: nativeEvent)
-                    }
-                }
+            if let theView = self.bridge.uiManager.view(forReactTag: node) as? THEOplayerRCTView {
+                theView.broadcastEventHandler.broadcastEvent(eventData: event)
             }
         }
     }
