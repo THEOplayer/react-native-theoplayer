47a177c1b9fcfad71dfc2ad109139570b18dc64d
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-01-24 12:43:28 +0100

    Use portal for fullscreen

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -22,15 +22,17 @@ import {
   TimeLabel,
   UiContainer,
 } from '@theoplayer/react-native-ui';
-import { PlayerConfiguration, PlayerEventType, THEOplayer, THEOplayerView } from 'react-native-theoplayer';
+import { PlayerConfiguration, PlayerEventType, PresentationMode, THEOplayer, THEOplayerView } from 'react-native-theoplayer';
 
-import { Platform, StyleSheet, View, ViewStyle } from 'react-native';
+import { useWindowDimensions, Modal, Platform, StyleSheet, Text, View } from 'react-native';
 import { SourceMenuButton, SOURCES } from './custom/SourceMenuButton';
 import { BackgroundAudioSubMenu } from './custom/BackgroundAudioSubMenu';
 import { PiPSubMenu } from './custom/PipSubMenu';
 import { MediaCacheDownloadButton } from './custom/MediaCacheDownloadButton';
 import { MediaCacheMenuButton } from './custom/MediaCacheMenuButton';
 import { MediaCachingTaskListSubMenu } from './custom/MediaCachingTaskListSubMenu';
+
+import { PortalDestination, PortalOrigin } from '@alexzunik/rn-native-portals-reborn';
 
 const playerConfig: PlayerConfiguration = {
   // Get your THEOplayer license from https://portal.theoplayer.com/
@@ -59,6 +61,25 @@ const playerConfig: PlayerConfiguration = {
  */
 export default function App() {
   const [player, setPlayer] = useState<THEOplayer | undefined>(undefined);
+  const [isFullScreenMode, setFullScreenModeState] = useState(false);
+  const { height, width } = useWindowDimensions();
+  const deviceWidth = width;
+  const deviceHeight = height;
+
+  const styles = StyleSheet.create({
+    videoContainer: {
+      width: '100%',
+      aspectRatio: '16/9',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#000000',
+    },
+    videoContainerFullscreen: {
+      width: deviceWidth,
+      height: deviceHeight,
+    },
+  });
+
   const chromeless = playerConfig?.chromeless ?? false;
   const onPlayerReady = (player: THEOplayer) => {
     setPlayer(player);
@@ -73,6 +94,10 @@ export default function App() {
     player.addEventListener(PlayerEventType.SEEKING, console.log);
     player.addEventListener(PlayerEventType.SEEKED, console.log);
     player.addEventListener(PlayerEventType.ENDED, console.log);
+    player.addEventListener(PlayerEventType.PRESENTATIONMODE_CHANGE, (event) => {
+      console.log('TVL', 'Fullscreen', event.presentationMode);
+      setFullScreenModeState(event.presentationMode === PresentationMode.fullscreen);
+    });
     player.source = SOURCES[0].source;
 
     player.backgroundAudioConfiguration = { enabled: true };
@@ -80,73 +105,72 @@ export default function App() {
     console.log('THEOplayer is ready:', player.version);
   };
 
-  const needsBorder = Platform.OS === 'ios';
-  const PLAYER_CONTAINER_STYLE: ViewStyle = {
-    position: 'absolute',
-    top: needsBorder ? 20 : 0,
-    left: needsBorder ? 5 : 0,
-    bottom: 0,
-    right: needsBorder ? 5 : 0,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#000000',
-  };
-
   return (
     <View style={[StyleSheet.absoluteFill, { backgroundColor: '#000000' }]}>
-      <View style={PLAYER_CONTAINER_STYLE}>
-        <THEOplayerView config={playerConfig} onPlayerReady={onPlayerReady}>
-          {player !== undefined && chromeless && (
-            <UiContainer
-              theme={{ ...DEFAULT_THEOPLAYER_THEME }}
-              player={player}
-              behind={<CenteredDelayedActivityIndicator size={50} />}
-              top={
-                <ControlBar>
-                  <MediaCacheMenuButton>
-                    <MediaCacheDownloadButton />
-                    <MediaCachingTaskListSubMenu />
-                  </MediaCacheMenuButton>
-                  {/*This is a custom menu for source selection.*/}
-                  <SourceMenuButton />
-                  {!Platform.isTV && (
-                    <>
-                      <AirplayButton />
-                      <ChromecastButton />
-                    </>
-                  )}
-                  <LanguageMenuButton />
-                  <SettingsMenuButton>
-                    {/*Note: quality selection is not available on iOS */}
-                    <QualitySubMenu />
-                    <PlaybackRateSubMenu />
-                    <BackgroundAudioSubMenu />
-                    <PiPSubMenu />
-                  </SettingsMenuButton>
-                </ControlBar>
-              }
-              center={<CenteredControlBar left={<SkipButton skip={-10} />} middle={<PlayButton />} right={<SkipButton skip={30} />} />}
-              bottom={
-                <>
-                  <ControlBar style={{ justifyContent: 'flex-start' }}>
-                    <CastMessage />
-                  </ControlBar>
+      <View style={isFullScreenMode ? styles.videoContainerFullscreen : styles.videoContainer}>
+        <PortalOrigin destination={isFullScreenMode ? 'fullscreen' : null} style={StyleSheet.absoluteFillObject}>
+          <THEOplayerView config={playerConfig} onPlayerReady={onPlayerReady}>
+            {player !== undefined && chromeless && (
+              <UiContainer
+                theme={{ ...DEFAULT_THEOPLAYER_THEME }}
+                player={player}
+                behind={<CenteredDelayedActivityIndicator size={50} />}
+                top={
                   <ControlBar>
-                    <SeekBar />
+                    <MediaCacheMenuButton>
+                      <MediaCacheDownloadButton />
+                      <MediaCachingTaskListSubMenu />
+                    </MediaCacheMenuButton>
+                    {/*This is a custom menu for source selection.*/}
+                    <SourceMenuButton />
+                    {!Platform.isTV && (
+                      <>
+                        <AirplayButton />
+                        <ChromecastButton />
+                      </>
+                    )}
+                    <LanguageMenuButton />
+                    <SettingsMenuButton>
+                      {/*Note: quality selection is not available on iOS */}
+                      <QualitySubMenu />
+                      <PlaybackRateSubMenu />
+                      <BackgroundAudioSubMenu />
+                      <PiPSubMenu />
+                    </SettingsMenuButton>
                   </ControlBar>
-                  <ControlBar>
-                    <MuteButton />
-                    <TimeLabel showDuration={true} />
-                    <Spacer />
-                    <PipButton />
-                    <FullscreenButton />
-                  </ControlBar>
-                </>
-              }
-            />
-          )}
-        </THEOplayerView>
+                }
+                center={<CenteredControlBar left={<SkipButton skip={-10} />} middle={<PlayButton />}
+                                            right={<SkipButton skip={30} />} />}
+                bottom={
+                  <>
+                    <ControlBar style={{ justifyContent: 'flex-start' }}>
+                      <CastMessage />
+                    </ControlBar>
+                    <ControlBar>
+                      <SeekBar />
+                    </ControlBar>
+                    <ControlBar>
+                      <MuteButton />
+                      <TimeLabel showDuration={true} />
+                      <Spacer />
+                      <PipButton />
+                      <FullscreenButton />
+                    </ControlBar>
+                  </>
+                }
+              />
+            )}
+          </THEOplayerView>
+        </PortalOrigin>
       </View>
+
+      <Text style={{ color: '#ffffff' }}>This text should not go into fullscreen</Text>
+
+      <Modal visible={isFullScreenMode}
+             statusBarTranslucent={true}
+             style={{ backgroundColor: '#000000', height: deviceHeight, width: deviceWidth }} animationType={'none'}>
+        <PortalDestination name="fullscreen" />
+      </Modal>
     </View>
   );
 }
