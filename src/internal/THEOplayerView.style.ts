a05ae8fe77098b35b78738bdee909d814f3aa3ed
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-02-08 14:22:52 +0100

    Remove unused style

--- src/internal/THEOplayerView.style.ts
+++ src/internal/THEOplayerView.style.ts
@@ -1,4 +1,4 @@
-import { Dimensions, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   base: {
@@ -9,8 +9,4 @@ export default StyleSheet.create({
     bottom: 0,
     right: 0,
   },
-  fullscreenOverride: {
-    width: Dimensions.get('screen').width,
-    height: Dimensions.get('screen').height
-  }
 });
