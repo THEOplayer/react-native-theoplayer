247ba9c031990ffed45dac8ba897a4df36256a64
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-02-22 09:24:03 +0100

    Allow audio ducking for transient focus loss

--- android/src/main/java/com/theoplayer/audio/AudioFocusManager.kt
+++ android/src/main/java/com/theoplayer/audio/AudioFocusManager.kt
@@ -17,6 +17,13 @@ private const val TAG = "AudioFocusManager"
  * Manages audio focus for the application, ensuring proper handling of audio focus changes
  * to control media playback behavior.
  *
+ * - Android 12 (API level 31) or later: Audio focus is managed by the system.
+ * - Android 8.0 (API level 26) through Android 11 (API level 30): Audio focus is not managed by
+ *   the system, but includes some changes that were introduced starting in Android.
+ * - Android 7.1 (API level 25) and lower: Audio focus is not managed by the system.
+ *
+ * @see <a href="https://developer.android.com/media/optimize/audio-focus">documentation</a>
+ *
  * @param context The context used to access system services.
  * @param player The media player instance associated with this audio focus manager. It can be
  *               provided optionally to control playback behavior.
@@ -29,22 +36,29 @@ class AudioFocusManager(
   private val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as? AudioManager
   private val uiModeManager = context.getSystemService(Context.UI_MODE_SERVICE) as? UiModeManager
   private var resumeOnFocusGain = false
+  private var originalVolume: Int = -1
   private val focusLock = Any()
 
   // Request for audio focus of unknown duration.
-  private val audioFocusRequest = AudioFocusRequestCompat.Builder(AudioManagerCompat.AUDIOFOCUS_GAIN)
-    .setAudioAttributes(
-      AudioAttributesCompat.Builder()
-        // Usage value to use when the usage is media, such as music, or movie soundtracks.
-        .setUsage(AudioAttributesCompat.USAGE_MEDIA)
-        // Content type value to use when the content type is a soundtrack, typically accompanying
-        // a movie or TV program.
-        .setContentType(AudioAttributesCompat.CONTENT_TYPE_MOVIE)
-        .build()
-    )
-    .setOnAudioFocusChangeListener(this)
-    .setWillPauseWhenDucked(true)
-    .build()
+  private val audioFocusRequest =
+    AudioFocusRequestCompat.Builder(AudioManagerCompat.AUDIOFOCUS_GAIN)
+      .setAudioAttributes(
+        AudioAttributesCompat.Builder()
+          // Usage value to use when the usage is media, such as music, or movie soundtracks.
+          .setUsage(AudioAttributesCompat.USAGE_MEDIA)
+          // Content type value to use when the content type is a soundtrack, typically accompanying
+          // a movie or TV program.
+          .setContentType(AudioAttributesCompat.CONTENT_TYPE_MOVIE)
+          .build()
+      )
+      .setOnAudioFocusChangeListener(this)
+
+      // Automatic ducking was introduced in Android 8.0 (API level 26)
+      // The audio system ducks our player(s) while the other app has focus. When the other app abandons
+      // focus, it unducks our player. We are not notified when on focus loss.
+      // https://developer.android.com/media/optimize/audio-focus#automatic_ducking
+      .setWillPauseWhenDucked(false)
+      .build()
 
   /**
    * Called on the listener to notify it the audio focus for this listener has been changed.
@@ -60,6 +74,7 @@ class AudioFocusManager(
     when (focusChange) {
       // Used to indicate a gain of audio focus, or a request of audio focus, of unknown duration.
       AudioManager.AUDIOFOCUS_GAIN -> {
+        // Optionally resume playback
         if (resumeOnFocusGain) {
           synchronized(focusLock) {
             // Reset resume flag
@@ -67,28 +82,42 @@ class AudioFocusManager(
           }
           player?.play()
         }
+
+        // Restore the audio level
+        if (originalVolume != -1) {
+          audioManager?.setStreamVolume(AudioManager.STREAM_MUSIC, originalVolume, 0)
+          originalVolume = -1
+        }
       }
 
       // Used to indicate a loss of audio focus of unknown duration.
       AudioManager.AUDIOFOCUS_LOSS -> {
-        synchronized (focusLock) {
-          // This is not a transient loss, we shouldn't automatically resume for now
-          resumeOnFocusGain = false;
+        synchronized(focusLock) {
+          // This is not a transient (short) loss, we shouldn't automatically resume for now.
+          resumeOnFocusGain = false
         }
+        player?.pause()
       }
 
-      // Used to indicate a transient loss of audio focus.
-      AudioManager.AUDIOFOCUS_LOSS_TRANSIENT,
-
-      // Used to indicate a transient loss of audio focus where the loser of the audio focus can
-      // lower its output volume if it wants to continue playing (also referred to as "ducking"),
-      // as the new focus owner doesn't require others to be silent.
-      AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
-        synchronized (focusLock) {
+      // Used to indicate a transient (short) loss of audio focus.
+      AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
+        synchronized(focusLock) {
           // We should only resume if playback was interrupted
           resumeOnFocusGain = !(player?.isPaused ?: true)
         }
         player?.pause()
+      }
+
+      // Used to indicate a transient (short) loss of audio focus where the loser of the audio focus
+      // can lower its output volume if it wants to continue playing (also referred to as "ducking"),
+      // as the new focus owner doesn't require others to be silent.
+      AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> {
+        synchronized(focusLock) {
+          audioManager?.apply {
+            originalVolume = getStreamVolume(AudioManager.STREAM_MUSIC)
+            setStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER, 0)
+          }
+        }
       }
     }
   }
@@ -107,7 +136,7 @@ class AudioFocusManager(
   }
 
   private fun fromAudioFocusRequest(focusRequest: Int?): String {
-    return when(focusRequest) {
+    return when (focusRequest) {
       AudioManager.AUDIOFOCUS_REQUEST_GRANTED -> "AUDIOFOCUS_REQUEST_GRANTED"
       AudioManager.AUDIOFOCUS_REQUEST_DELAYED -> "AUDIOFOCUS_REQUEST_DELAYED"
       else -> "AUDIOFOCUS_REQUEST_FAILED"
@@ -133,7 +162,7 @@ class AudioFocusManager(
    * Abandon audio focus. Causes the previous focus owner, if any, to receive focus.
    */
   fun abandonAudioFocus() {
-    synchronized (focusLock) {
+    synchronized(focusLock) {
       resumeOnFocusGain = false
     }
     val result = audioManager?.let {
