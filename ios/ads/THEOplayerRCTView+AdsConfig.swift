3fd6f822c46160dcb7b5b244378db9b14340c60f
William Van Haevre <william.vanhaevre@gmail.com>
2024-03-07 16:06:01 +0100

    Parse imaSettings from PlayerConfig on iOS bridge

--- ios/ads/THEOplayerRCTView+AdsConfig.swift
+++ ios/ads/THEOplayerRCTView+AdsConfig.swift
@@ -6,6 +6,16 @@ import THEOplayerSDK
 struct AdsConfig {
     var adSUIEnabled: Bool = true
     var adPreloadTypeString: String = "none"
+    var adsImaConfig = AdsImaConfig()
+}
+
+struct AdsImaConfig {
+    var maxRedirects: UInt = 4
+    var enableDebugMode: Bool = false
+    var ppid: String?
+    var featureFlags: [String:String]?
+    var autoPlayAdBreaks: Bool?
+    var sessionID: String?
 }
 
 #if os(iOS)
@@ -17,6 +27,26 @@ extension THEOplayerRCTView {
             self.adsConfig.adSUIEnabled = adsConfig["uiEnabled"] as? Bool ?? true
             if let adPreloadType = adsConfig["preload"] as? String {
                 self.adsConfig.adPreloadTypeString = adPreloadType
+            }
+            if let adsImaConfig = adsConfig["ima"] as? NSDictionary {
+                if let ppid = adsImaConfig["ppid"] as? String {
+                    self.adsConfig.adsImaConfig.ppid = ppid
+                }
+                if let maxRedirects = adsImaConfig["maxRedirects"] as? UInt {
+                    self.adsConfig.adsImaConfig.maxRedirects = maxRedirects
+                }
+                if let featureFlags = adsImaConfig["featureFlags"] as? [String:String] {
+                    self.adsConfig.adsImaConfig.featureFlags = featureFlags
+                }
+                if let autoPlayAdBreaks = adsImaConfig["autoPlayAdBreaks"] as? Bool {
+                    self.adsConfig.adsImaConfig.autoPlayAdBreaks = autoPlayAdBreaks
+                }
+                if let sessionID = adsImaConfig["sessionID"] as? String {
+                    self.adsConfig.adsImaConfig.sessionID = sessionID
+                }
+                if let enableDebugMode = adsImaConfig["enableDebugMode"] as? Bool {
+                    self.adsConfig.adsImaConfig.enableDebugMode = enableDebugMode
+                }
             }
         }
     }
