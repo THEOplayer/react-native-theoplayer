84ac715b24e61c63a1ce5c6233d185c77e61a7f1
William Van Haevre <william.vanhaevre@gmail.com>
2024-03-07 14:54:13 +0100

    Drop unused usaNativeIma setting and add the supported IMA settings for iOS and Android

--- src/api/ads/GoogleImaConfiguration.ts
+++ src/api/ads/GoogleImaConfiguration.ts
@@ -5,10 +5,42 @@
  */
 export interface GoogleImaConfiguration {
   /**
-   * Whether the native (mobile) IMA implementation will be used.
-   *
-   * @deprecated This property applies to native web-based player SDKs and will be removed.
-   * @internal
+   * Publisher Provided Identification (PPID) sent with ads request.
    */
-  useNativeIma: boolean;
+  ppid?: string;
+
+  /**
+   * Specifies maximum number of redirects after which subsequent redirects will
+   * be denied, and the ad load aborted. The number of redirects directly affects
+   * latency and thus user experience. This applies to all VAST wrapper ads. If
+   * the number of redirects exceeds |maxRedirects|, the ad request will fail with
+   * error code 302.
+   * @defaultValue `4`
+   */
+  maxRedirects?: number;
+
+  /**
+   * Feature flags and their states. Used to control experimental features.
+   */
+  featureFlags?: { [flag: string]: string };
+
+  /**
+   * Specifies whether to automatically play VMAP and ad rules ad breaks. The
+   * @defaultValue `true`
+   */
+  autoPlayAdBreaks?: boolean;
+
+  /**
+   * The session ID to identify a single user session. This should be a UUID. It
+   * is used exclusively for frequency capping across the user session.
+   */
+  sessionID?: string;
+
+  /**
+   * Toggles debug mode which will output detailed log information to the console.
+   * Debug mode should be disabled in Release and will display a watermark when
+   * enabled.
+   * @defaultValue `false`
+   */
+  enableDebugMode?: boolean;
 }
