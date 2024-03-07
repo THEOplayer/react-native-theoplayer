8d78df6d8a52fc6a8f06bb229ab1bc70ee4c5345
William Van Haevre <william.vanhaevre@gmail.com>
2024-03-07 16:06:27 +0100

    Pass collected ImaSettings to AdsIntegration on iOS

--- ios/ads/THEOplayerRCTView+Ads.swift
+++ ios/ads/THEOplayerRCTView+Ads.swift
@@ -5,6 +5,7 @@ import THEOplayerSDK
 
 #if canImport(THEOplayerGoogleIMAIntegration)
 import THEOplayerGoogleIMAIntegration
+import GoogleInteractiveMediaAds
 #endif
 
 extension THEOplayerRCTView {
@@ -21,7 +22,28 @@ extension THEOplayerRCTView {
             return
         }
 #if canImport(THEOplayerGoogleIMAIntegration)
-        let imaIntegration = GoogleIMAIntegrationFactory.createIntegration(on: player)
+        // prepare imaSettings
+        let imaSettings = IMASettings()
+        imaSettings.language = self.uiConfig.language
+        imaSettings.maxRedirects = self.adsConfig.adsImaConfig.maxRedirects
+        imaSettings.enableDebugMode = self.adsConfig.adsImaConfig.enableDebugMode
+        imaSettings.playerType = "THEOplayer"
+        imaSettings.playerVersion = THEOplayer.version
+        if let ppid = self.adsConfig.adsImaConfig.ppid {
+            imaSettings.ppid = ppid
+        }
+        if let featureFlags = self.adsConfig.adsImaConfig.featureFlags {
+            imaSettings.featureFlags = featureFlags
+        }
+        if let autoPlayAdBreaks = self.adsConfig.adsImaConfig.autoPlayAdBreaks {
+            imaSettings.autoPlayAdBreaks = autoPlayAdBreaks
+        }
+        if let sessionID = self.adsConfig.adsImaConfig.sessionID {
+            imaSettings.sessionID = sessionID
+        }
+        
+        // setup integration
+        let imaIntegration = GoogleIMAIntegrationFactory.createIntegration(on: player, with: imaSettings)
         player.addIntegration(imaIntegration)
 #endif
     }
