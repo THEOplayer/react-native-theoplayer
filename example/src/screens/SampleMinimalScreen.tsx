b85f22d1c7a5e767732fb8f2ffe9f867f959aa72
William Van Haevre <william.vanhaevre@gmail.com>
2024-03-25 15:27:22 +0100

    Simplified minimal setup to source and play command

--- example/src/screens/SampleMinimalScreen.tsx
+++ example/src/screens/SampleMinimalScreen.tsx
@@ -1,49 +1,42 @@
 import * as React from 'react';
 import { useState } from 'react';
-import {
-  CenteredControlBar,
-  CenteredDelayedActivityIndicator,
-  ControlBar,
-  DEFAULT_THEOPLAYER_THEME,
-  MuteButton,
-  PlayButton,
-  SeekBar,
-  SkipButton,
-  TimeLabel,
-  UiContainer,
-} from '@theoplayer/react-native-ui';
-import { PlayerConfiguration, PlayerEventType, THEOplayer, THEOplayerView } from 'react-native-theoplayer';
+import { PlayerConfiguration, THEOplayer, THEOplayerView } from 'react-native-theoplayer';
 
 import { Platform, SafeAreaView, StyleSheet, View, ViewStyle } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { SourceMenuButton, SOURCES } from '../custom/SourceMenuButton';
+import {usePlayerFocus} from "../hooks/usePlayerFocus";
+
+const LOG_TAG = "[EXAMPLE - MINIMAL SAMPLE]";
 
 const playerConfig: PlayerConfiguration = {
   // Get your THEOplayer license from https://portal.theoplayer.com/
   // Without a license, only demo sources hosted on '*.theoplayer.com' domains can be played.
+  license: undefined,
   libraryLocation: 'theoplayer',
 };
 
 export default function SampleMinimalScreen() {
   const [player, setPlayer] = useState<THEOplayer | undefined>(undefined);
   const onPlayerReady = (player: THEOplayer) => {
+    console.log(LOG_TAG, 'THEOplayer is ready:', player.version);
     setPlayer(player);
 
-    // optional debug logs
-    player.addEventListener<PlayerEventType>(PlayerEventType.SOURCE_CHANGE, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.LOADED_DATA, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.LOADED_METADATA, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.READYSTATE_CHANGE, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.PLAY, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.PLAYING, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.PAUSE, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.SEEKING, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.SEEKED, console.log);
-    player.addEventListener<PlayerEventType>(PlayerEventType.ENDED, console.log);
-    player.source = SOURCES[0].source;
+    // set a source
+    player.source = {
+      sources: [
+        {
+          "src": "https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8",
+          "type": "application/x-mpegurl"
+        }
+      ]
+    };
 
-    console.log('THEOplayer is ready:', player.version);
+    // start playing
+    player.play();
   };
+
+  // example app specific setup
+  usePlayerFocus(player);
 
   const PLAYER_CONTAINER_STYLE: ViewStyle = {
     position: 'absolute',
@@ -59,37 +52,7 @@ export default function SampleMinimalScreen() {
   return (
     <SafeAreaView style={[StyleSheet.absoluteFill, { backgroundColor: '#000000' }]}>
       <View style={PLAYER_CONTAINER_STYLE}>
-        <THEOplayerView config={playerConfig} onPlayerReady={onPlayerReady}>
-          {player !== undefined && (
-            <UiContainer
-              theme={{ ...DEFAULT_THEOPLAYER_THEME }}
-              player={player}
-              behind={<CenteredDelayedActivityIndicator size={50} />}
-              top={
-                <ControlBar>
-                  <SourceMenuButton />
-                </ControlBar>
-              }
-              center={<CenteredControlBar left={<SkipButton skip={-10} />} middle={<PlayButton />} right={<SkipButton skip={30} />} />}
-              bottom={
-                <>
-                  {
-                    /*Note: RNSlider is not available on tvOS */
-                    !(Platform.isTV && Platform.OS === 'ios') && (
-                      <ControlBar>
-                        <SeekBar />
-                      </ControlBar>
-                    )
-                  }
-                  <ControlBar>
-                    <MuteButton />
-                    <TimeLabel showDuration={true} />
-                  </ControlBar>
-                </>
-              }
-            />
-          )}
-        </THEOplayerView>
+        <THEOplayerView config={playerConfig} onPlayerReady={onPlayerReady} />
       </View>
     </SafeAreaView>
   );
