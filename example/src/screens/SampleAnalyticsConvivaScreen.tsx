c7dbb864af682a1c4e03a2b2889417ec5e596e0d
William Van Haevre <william.vanhaevre@gmail.com>
2024-04-29 12:05:26 +0200

    Add basic controls to analytics samples

--- example/src/screens/SampleAnalyticsConvivaScreen.tsx
+++ example/src/screens/SampleAnalyticsConvivaScreen.tsx
@@ -3,10 +3,20 @@ import { useState } from 'react';
 import { PlayerConfiguration, THEOplayer, THEOplayerView } from 'react-native-theoplayer';
 import { Platform, SafeAreaView, StyleSheet, View } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { useConviva, ConvivaMetadata } from "@theoplayer/react-native-analytics-conviva";
-import {CONVIVA_CONFIG, CONVIVA_CUSTOMER_KEY, THEO_LICENSE} from "../sampleConfig";
+import { useConviva, ConvivaMetadata } from '@theoplayer/react-native-analytics-conviva';
+import { CONVIVA_CONFIG, CONVIVA_CUSTOMER_KEY, THEO_LICENSE } from '../sampleConfig';
+import {
+  CenteredControlBar,
+  CenteredDelayedActivityIndicator,
+  ControlBar,
+  DEFAULT_THEOPLAYER_THEME,
+  PlayButton,
+  SeekBar,
+  SkipButton,
+  UiContainer,
+} from '@theoplayer/react-native-ui';
 
-const LOG_TAG = "[EXAMPLE - ANALYTICS CONVIVA SAMPLE]";
+const LOG_TAG = '[EXAMPLE - ANALYTICS CONVIVA SAMPLE]';
 const playerConfig: PlayerConfiguration = {
   license: THEO_LICENSE,
   libraryLocation: 'theoplayer',
@@ -15,11 +25,11 @@ const playerConfig: PlayerConfiguration = {
 const convivaMetadata: ConvivaMetadata = {
   ['Conviva.applicationName']: 'THEOplayer',
   ['Conviva.viewerId']: 'your_viewer_id',
-  ['customTag1']: "customValue1",
+  ['customTag1']: 'customValue1',
 };
 
 export const SampleAnalyticsConvivaScreen = () => {
-  const [, setPlayer] = useState<THEOplayer | undefined>(undefined);
+  const [player, setPlayer] = useState<THEOplayer | undefined>(undefined);
   const [, initConviva] = useConviva(convivaMetadata, CONVIVA_CONFIG);
 
   const onPlayerReady = (player: THEOplayer) => {
@@ -37,13 +47,13 @@ export const SampleAnalyticsConvivaScreen = () => {
     player.source = {
       sources: [
         {
-          "src": "https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8",
-          "type": "application/x-mpegurl"
-        }
+          src: 'https://cdn.theoplayer.com/video/big_buck_bunny/big_buck_bunny.m3u8',
+          type: 'application/x-mpegurl',
+        },
       ],
       metadata: {
-        title: "My metadata title"
-      }
+        title: 'My metadata title',
+      },
     };
 
     // start playing
@@ -53,11 +63,32 @@ export const SampleAnalyticsConvivaScreen = () => {
   return (
     <SafeAreaView style={[StyleSheet.absoluteFill, { backgroundColor: '#000000' }]}>
       <View style={styles.PLAYER_CONTAINER_STYLE}>
-        <THEOplayerView config={playerConfig} onPlayerReady={onPlayerReady} />
+        <THEOplayerView config={playerConfig} onPlayerReady={onPlayerReady}>
+          {player !== undefined && (
+            <UiContainer
+              theme={{ ...DEFAULT_THEOPLAYER_THEME }}
+              player={player}
+              behind={<CenteredDelayedActivityIndicator size={50} />}
+              center={<CenteredControlBar left={<SkipButton skip={-10} />} middle={<PlayButton />} right={<SkipButton skip={30} />} />}
+              bottom={
+                <>
+                  {
+                    /*Note: RNSlider is not available on tvOS */
+                    !(Platform.isTV && Platform.OS === 'ios') && (
+                      <ControlBar>
+                        <SeekBar />
+                      </ControlBar>
+                    )
+                  }
+                </>
+              }
+            />
+          )}
+        </THEOplayerView>
       </View>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   PLAYER_CONTAINER_STYLE: {
