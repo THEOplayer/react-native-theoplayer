bd3b058440351eec3279aa12b494c82330f78637
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-06-20 11:17:15 +0200

    Add forwarded ad events

--- android/src/main/java/com/theoplayer/ads/AdEventAdapter.kt
+++ android/src/main/java/com/theoplayer/ads/AdEventAdapter.kt
@@ -13,32 +13,12 @@ import com.theoplayer.android.api.ads.ima.GoogleImaAdEventType
 import com.theoplayer.android.api.ads.wrapper.AdEventListener
 import com.theoplayer.android.api.event.EventType
 import com.theoplayer.android.api.event.ads.AdEvent
+import com.theoplayer.android.api.event.ads.AdsEventTypes
 import java.util.*
 
 private const val EVENT_PROP_AD = "ad"
 private const val EVENT_PROP_TYPE = "type"
 private const val EVENT_PROP_SUBTYPE = "subType"
-
-private val ALL_AD_EVENTS = arrayOf(
-  GoogleImaAdEventType.LOADED,
-  GoogleImaAdEventType.AD_BREAK_STARTED,
-  GoogleImaAdEventType.STARTED,
-  GoogleImaAdEventType.FIRST_QUARTILE,
-  GoogleImaAdEventType.MIDPOINT,
-  GoogleImaAdEventType.THIRD_QUARTILE,
-  GoogleImaAdEventType.COMPLETED,
-  GoogleImaAdEventType.AD_BREAK_ENDED,
-  GoogleImaAdEventType.SKIPPED,
-  GoogleImaAdEventType.AD_ERROR,
-  GoogleImaAdEventType.AD_BUFFERING,
-  GoogleImaAdEventType.AD_BREAK_FETCH_ERROR,
-  GoogleImaAdEventType.CONTENT_PAUSE_REQUESTED,
-  GoogleImaAdEventType.CONTENT_RESUME_REQUESTED,
-  GoogleImaAdEventType.CLICKED,
-  GoogleImaAdEventType.TAPPED,
-  GoogleImaAdEventType.ICON_TAPPED,
-  GoogleImaAdEventType.ICON_FALLBACK_IMAGE_CLOSED,
-)
 
 class AdEventAdapter(private val adsApi: AdsApiWrapper, eventEmitter: AdEventEmitter) {
   private val eventListener: AdEventListener
@@ -71,9 +51,7 @@ class AdEventAdapter(private val adsApi: AdsApiWrapper, eventEmitter: AdEventEmi
         eventEmitter.emit(payload)
       }
     }
-    for (eventType in ALL_AD_EVENTS) {
-      adsApi.addEventListener(eventType, eventListener)
-    }
+    adsApi.addAllEventsListener(eventListener)
   }
 
   companion object {
@@ -123,33 +101,36 @@ class AdEventAdapter(private val adsApi: AdsApiWrapper, eventEmitter: AdEventEmi
     }
 
     private fun mapAdType(eventType: EventType<*>): String {
-      return when (eventType as GoogleImaAdEventType) {
-        GoogleImaAdEventType.LOADED -> "adloaded"
-        GoogleImaAdEventType.STARTED -> "adbegin"
-        GoogleImaAdEventType.FIRST_QUARTILE -> "adfirstquartile"
-        GoogleImaAdEventType.MIDPOINT -> "admidpoint"
-        GoogleImaAdEventType.THIRD_QUARTILE -> "adthirdquartile"
-        GoogleImaAdEventType.COMPLETED -> "adend"
-        GoogleImaAdEventType.SKIPPED -> "adskip"
-        GoogleImaAdEventType.AD_ERROR -> "aderror"
+      return when (eventType) {
+        GoogleImaAdEventType.LOADED, AdsEventTypes.AD_LOADED -> "adloaded"
+        GoogleImaAdEventType.STARTED, AdsEventTypes.AD_BEGIN -> "adbegin"
+        GoogleImaAdEventType.FIRST_QUARTILE, AdsEventTypes.AD_FIRST_QUARTILE -> "adfirstquartile"
+        GoogleImaAdEventType.MIDPOINT, AdsEventTypes.AD_MIDPOINT -> "admidpoint"
+        GoogleImaAdEventType.THIRD_QUARTILE, AdsEventTypes.AD_THIRD_QUARTILE -> "adthirdquartile"
+        GoogleImaAdEventType.COMPLETED, AdsEventTypes.AD_END -> "adend"
+        GoogleImaAdEventType.SKIPPED, AdsEventTypes.AD_SKIP -> "adskip"
+        GoogleImaAdEventType.AD_ERROR, AdsEventTypes.AD_ERROR -> "aderror"
         GoogleImaAdEventType.AD_BUFFERING -> "adbuffering"
         GoogleImaAdEventType.CONTENT_PAUSE_REQUESTED -> "adbreakbegin"
         GoogleImaAdEventType.CONTENT_RESUME_REQUESTED -> "adbreakend"
-        GoogleImaAdEventType.AD_BREAK_STARTED -> "adbreakbegin"
-        GoogleImaAdEventType.AD_BREAK_ENDED -> "adbreakend"
+        GoogleImaAdEventType.AD_BREAK_STARTED, AdsEventTypes.AD_BREAK_BEGIN -> "adbreakbegin"
+        GoogleImaAdEventType.AD_BREAK_ENDED, AdsEventTypes.AD_BREAK_END -> "adbreakend"
         GoogleImaAdEventType.AD_BREAK_FETCH_ERROR -> "aderror"
         GoogleImaAdEventType.CLICKED -> "adclicked"
         GoogleImaAdEventType.TAPPED -> "adtapped"
         GoogleImaAdEventType.ICON_TAPPED -> "adicontapped"
         GoogleImaAdEventType.ICON_FALLBACK_IMAGE_CLOSED -> "adiconfallbackimageclosed"
-        else -> eventType.getName().lowercase(Locale.getDefault())
+        AdsEventTypes.ADD_AD -> "addad"
+        AdsEventTypes.AD_IMPRESSION -> "adimpression"
+        AdsEventTypes.ADD_AD_BREAK -> "addadbreak"
+        AdsEventTypes.REMOVE_AD_BREAK -> "removeadbreak"
+        AdsEventTypes.AD_BREAK_CHANGE -> "adbreakchange"
+        else -> eventType.name.lowercase(Locale.getDefault())
       }
     }
   }
 
   fun destroy() {
-    for (eventType in ALL_AD_EVENTS) {
-      adsApi.removeEventListener(eventType, eventListener)
-    }
+    adsApi.removeAllEventsListener(eventListener)
   }
 }
