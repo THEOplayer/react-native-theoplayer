45985ec681802c668df6212674902d91da1b9608
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-06-27 12:48:15 +0200

    Refactor custom menus

--- example/src/custom/BackgroundAudioSubMenu.tsx
+++ example/src/custom/BackgroundAudioSubMenu.tsx
@@ -1,6 +1,8 @@
-import React, { useContext, useState } from 'react';
+import { CustomSubMenu, Option } from './CustomSubMenu';
+import * as React from 'react';
 import type { StyleProp, ViewStyle } from 'react-native';
-import { MenuRadioButton, MenuView, PlayerContext, ScrollableMenu, SubMenuWithButton } from '@theoplayer/react-native-ui';
+import { useContext } from 'react';
+import { PlayerContext } from '@theoplayer/react-native-ui/src/ui/components/util/PlayerContext';
 
 export interface BackgroundAudioSubMenuProps {
   /**
@@ -9,61 +11,21 @@ export interface BackgroundAudioSubMenuProps {
   menuStyle?: StyleProp<ViewStyle>;
 }
 
-interface Option {
-  label: string;
-  value: boolean;
-}
-
-const OptionDisabled = { label: 'Disabled', value: false };
-const OptionEnabled = { label: 'Enabled', value: true };
-const options: Option[] = [OptionDisabled, OptionEnabled];
-
 /**
  * A button component that opens a backgroundAudio selection menu for the `react-native-theoplayer` UI.
  */
-export const BackgroundAudioSubMenu = (props: BackgroundAudioSubMenuProps) => {
-  const { menuStyle } = props;
-  const createMenu = () => {
-    return <BackgroundAudioSelectionView style={menuStyle} />;
-  };
-  const player = useContext(PlayerContext).player;
-  const selectedLabel = player.backgroundAudioConfiguration.enabled ? OptionEnabled.label : OptionDisabled.label;
-  return <SubMenuWithButton menuConstructor={createMenu} label={'Bg Audio'} preview={selectedLabel} />;
-};
-
-export interface BackgroundAudioSelectionViewProps {
-  style?: StyleProp<ViewStyle>;
-}
-
-const BackgroundAudioSelectionView = (props: BackgroundAudioSelectionViewProps) => {
-  const { style } = props;
+export function BackgroundAudioSubMenu(props?: BackgroundAudioSubMenuProps) {
   const player = useContext(PlayerContext).player;
 
-  const [selected, setSelected] = useState<Option>(player.backgroundAudioConfiguration.enabled ? OptionEnabled : OptionDisabled);
-
-  const onSelectOption = (id: number | undefined): void => {
-    if (id !== undefined) {
-      player.backgroundAudioConfiguration = { enabled: options[id].value };
-      setSelected(options[id]);
-    }
-  };
-
-  return (
-    <MenuView
-      style={style}
-      menu={
-        <ScrollableMenu
-          title={'Background Audio'}
-          items={options.map((option, id) => (
-            <MenuRadioButton
-              key={id}
-              label={option.label}
-              uid={id}
-              onSelect={onSelectOption}
-              selected={selected.value === option.value}></MenuRadioButton>
-          ))}
-        />
+  return <CustomSubMenu
+    title={'Background Audio'}
+    menuStyle={props?.menuStyle}
+    label={'Bg Audio'}
+    options={[{ label: 'Disabled', value: false }, { label: 'Enabled', value: true }]}
+    onOptionSelected={(option: Option<boolean>) => {
+      if (player) {
+        player.backgroundAudioConfiguration = { enabled: option.value };
       }
-    />
-  );
-};
+    }}
+    currentOption={() => player.pipConfiguration.startsAutomatically ?? false} />;
+}
--- example/src/custom/PipSubMenu.tsx
+++ example/src/custom/PipSubMenu.tsx
@@ -1,6 +1,8 @@
-import React, { useContext, useState } from 'react';
+import { CustomSubMenu, Option } from './CustomSubMenu';
+import * as React from 'react';
 import type { StyleProp, ViewStyle } from 'react-native';
-import { MenuRadioButton, MenuView, PlayerContext, ScrollableMenu, SubMenuWithButton } from '@theoplayer/react-native-ui';
+import { useContext } from 'react';
+import { PlayerContext } from '@theoplayer/react-native-ui/src/ui/components/util/PlayerContext';
 
 export interface PipSubMenuProps {
   /**
@@ -9,61 +11,18 @@ export interface PipSubMenuProps {
   menuStyle?: StyleProp<ViewStyle>;
 }
 
-interface Option {
-  label: string;
-  value: boolean;
-}
-
-const OptionAutoDisabled = { label: 'Disabled', value: false };
-const OptionAutoEnabled = { label: 'Enabled', value: true };
-const options: Option[] = [OptionAutoDisabled, OptionAutoEnabled];
-
-/**
- * A button component that opens a PiP configuration selection menu for the `react-native-theoplayer` UI.
- */
-export const PiPSubMenu = (props: PipSubMenuProps) => {
-  const { menuStyle } = props;
-  const createMenu = () => {
-    return <PiPSelectionView style={menuStyle} />;
-  };
-  const player = useContext(PlayerContext).player;
-  const selectedLabel = player.pipConfiguration.startsAutomatically ? OptionAutoEnabled.label : OptionAutoDisabled.label;
-  return <SubMenuWithButton menuConstructor={createMenu} label={'Auto PiP'} preview={selectedLabel} />;
-};
-
-export interface PiPSelectionViewProps {
-  style?: StyleProp<ViewStyle>;
-}
-
-const PiPSelectionView = (props: PiPSelectionViewProps) => {
-  const { style } = props;
+export function PiPSubMenu(props?: PipSubMenuProps) {
   const player = useContext(PlayerContext).player;
 
-  const [selected, setSelected] = useState<Option>(player.pipConfiguration.startsAutomatically ? OptionAutoEnabled : OptionAutoDisabled);
-
-  const onSelectOption = (id: number | undefined): void => {
-    if (id !== undefined) {
-      player.pipConfiguration = { startsAutomatically: options[id].value };
-      setSelected(options[id]);
-    }
-  };
-
-  return (
-    <MenuView
-      style={style}
-      menu={
-        <ScrollableMenu
-          title={'Automatically Start Picture-in-Picture'}
-          items={options.map((option, id) => (
-            <MenuRadioButton
-              key={id}
-              label={option.label}
-              uid={id}
-              onSelect={onSelectOption}
-              selected={selected.value === option.value}></MenuRadioButton>
-          ))}
-        />
+  return <CustomSubMenu
+    title={'Automatically Start Picture-in-Picture'}
+    menuStyle={props?.menuStyle}
+    label={'Auto PiP'}
+    options={[{ label: 'Disabled', value: false }, { label: 'Enabled', value: true }]}
+    onOptionSelected={(option: Option<boolean>) => {
+      if (player) {
+        player.pipConfiguration = { startsAutomatically: option.value };
       }
-    />
-  );
-};
+    }}
+    currentOption={() => player.pipConfiguration.startsAutomatically ?? false} />;
+}
