a7aa768c7dc991d82fa3dac99dcaefbd01bf7a04
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-10-18 00:41:42 +0200

    Fix ad tests for iOS

--- e2e/src/tests/Ads.spec.ts
+++ e2e/src/tests/Ads.spec.ts
@@ -3,29 +3,25 @@ import { AdDescription, PlayerEventType, SourceDescription, THEOplayer } from 'r
 import { applyActionAndExpectPlayerEvents, failOnPlayerError } from '../utils/Actions';
 import hls from '../res/hls.json';
 import ads from '../res/ads.json';
-import { Platform } from 'react-native';
 
 function extendSourceWithAds(source: SourceDescription, ad: AdDescription): SourceDescription {
-  return {...source, ads: [ad]};
+  return { ...source, ads: [ad] };
 }
 
-export default function(spec: TestScope) {
-  // TODO: flaky on iOS
-  if (Platform.OS !== 'ios') {
-    spec.describe('Set source with ads and auto-play', function() {
-      spec.it('dispatches sourcechange, play, playing and ad events', async function() {
-        // We are not epecting any player errors.
-        await failOnPlayerError();
+export default function (spec: TestScope) {
+  spec.describe('Set source with ads and auto-play', function () {
+    spec.it('dispatches sourcechange, play, playing and ad events', async function () {
+      // We are not epecting any player errors.
+      await failOnPlayerError();
 
-        // Start autoplay and expect events.
-        await applyActionAndExpectPlayerEvents(
-          (player: THEOplayer) => {
-            player.autoplay = true;
-            player.source = extendSourceWithAds(hls[0], ads[0] as AdDescription);
-          },
-          [PlayerEventType.SOURCE_CHANGE, PlayerEventType.PLAY, PlayerEventType.PLAYING, PlayerEventType.AD_EVENT],
-        );
-      });
+      // Start autoplay and expect events.
+      await applyActionAndExpectPlayerEvents(
+        (player: THEOplayer) => {
+          player.autoplay = true;
+          player.source = extendSourceWithAds(hls[0], ads[0] as AdDescription);
+        },
+        [PlayerEventType.SOURCE_CHANGE, PlayerEventType.PLAY, PlayerEventType.PLAYING, PlayerEventType.AD_EVENT],
+      );
     });
-  }
+  });
 }
