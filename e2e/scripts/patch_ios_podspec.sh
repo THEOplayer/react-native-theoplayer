84e92feb568b8e42a525ea898eb54d63d5770f58
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-10-18 00:41:42 +0200

    Fix ad tests for iOS

---
+++ e2e/scripts/patch_ios_podspec.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILE='../react-native-theoplayer.podspec'
+
+if [[ ! -f $FILE ]]; then
+  echo "File not found!"
+  exit 1
+fi
+
+# Replace '/../..' with '.' to find the file react-native-theoplayer.json
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # For macOS, use sed with an empty backup extension
+  sed -i '' 's|/\.\./\.\.|\.|g' "$FILE"
+else
+  # For Linux/Ubuntu, no backup extension is required
+  sed -i 's|/\.\./\.\.|\.|g' "$FILE"
+fi
+
+echo "Replacement done in $FILE"
