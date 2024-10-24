6240989cf2010d600d7a15f849fa3b3d514a5f26
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-10-24 10:40:07 +0200

    Modify e2e project

--- e2e/src/tests/index.ts
+++ e2e/src/tests/index.ts
@@ -1,7 +1,7 @@
 import Ads from './Ads.spec';
 import Basic from './Basic.spec';
-import Connector from './Connector.spec';
+// import Connector from './Connector.spec';
 import PresentationMode from './PresentationMode.spec';
 import Version from './Version.spec';
 
-export default [Version, Basic, Ads, Connector, PresentationMode];
+export default [Version, Basic, Ads, /*Connector,*/ PresentationMode];
