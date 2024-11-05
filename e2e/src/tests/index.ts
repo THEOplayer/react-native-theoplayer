b3e90cd76335c5f7706d3d3800de6ce85a23b19f
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-11-05 12:52:50 +0100

    Add versions e2e test

--- e2e/src/tests/index.ts
+++ e2e/src/tests/index.ts
@@ -2,5 +2,6 @@ import Ads from './Ads.spec';
 import Basic from './Basic.spec';
 import Connector from './Connector.spec';
 import PresentationMode from './PresentationMode.spec';
+import Version from './Version.spec';
 
-export default [Basic, Ads, Connector, PresentationMode];
+export default [Basic, Ads, Connector, PresentationMode, Version];
