071d4ee52881dccb76c8e7cb1ee52e653337367b
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-11-06 13:50:15 +0100

    Add codegen support

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+const pkg = require('../package.json');
+
 module.exports = {
   dependencies: {
     'react-native-google-cast': {
       platforms: {
         ios: null, // this will disable autolinking for this package on iOS
       },
+    },
+    [pkg.name]: {
+      root: path.join(__dirname, '..'),
     },
   },
 };
