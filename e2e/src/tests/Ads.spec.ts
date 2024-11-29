deb932bb8f0e899962353ed813539955e0459bc8
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-11-29 09:56:44 +0100

    Use test source builder

--- e2e/src/tests/Ads.spec.ts
+++ e2e/src/tests/Ads.spec.ts
@@ -4,29 +4,34 @@ import hls from '../res/hls.json';
 import ads from '../res/ads.json';
 import { getTestPlayer } from '../components/TestableTHEOplayerView';
 import { waitForPlayerEvents, waitForPlayerEventTypes } from '../utils/Actions';
+import { TestSourceDescription, TestSources } from '../utils/SourceUtils';
 
 function extendSourceWithAds(source: SourceDescription, ad: AdDescription): SourceDescription {
   return { ...source, ads: [ad] };
 }
 
 export default function (spec: TestScope) {
-  spec.describe('Set source with ads and auto-play', function () {
-    spec.it('dispatches sourcechange, play, playing and ad events', async function () {
-      const player = await getTestPlayer();
-      const playEventsPromise = waitForPlayerEventTypes(player, [PlayerEventType.SOURCE_CHANGE, PlayerEventType.PLAY, PlayerEventType.PLAYING]);
+  TestSources()
+    .withAds()
+    .forEach((testSource: TestSourceDescription) => {
+      spec.describe(`Set ${testSource.description} and auto-play`, function () {
+        spec.it('dispatches sourcechange, play, playing and ad events', async function () {
+          const player = await getTestPlayer();
+          const playEventsPromise = waitForPlayerEventTypes(player, [PlayerEventType.SOURCE_CHANGE, PlayerEventType.PLAY, PlayerEventType.PLAYING]);
 
-      const adEventsPromise = waitForPlayerEvents(player, [
-        { type: PlayerEventType.AD_EVENT, subType: AdEventType.AD_BREAK_BEGIN } as AdEvent,
-        { type: PlayerEventType.AD_EVENT, subType: AdEventType.AD_BEGIN } as AdEvent,
-      ]);
+          const adEventsPromise = waitForPlayerEvents(player, [
+            { type: PlayerEventType.AD_EVENT, subType: AdEventType.AD_BREAK_BEGIN } as AdEvent,
+            { type: PlayerEventType.AD_EVENT, subType: AdEventType.AD_BEGIN } as AdEvent,
+          ]);
 
-      // Start autoplay
-      player.autoplay = true;
-      player.source = extendSourceWithAds(hls[0], ads[0] as AdDescription);
+          // Start autoplay
+          player.autoplay = true;
+          player.source = extendSourceWithAds(hls[0], ads[0] as AdDescription);
 
-      // Expect events.
-      await playEventsPromise;
-      await adEventsPromise;
+          // Expect events.
+          await playEventsPromise;
+          await adEventsPromise;
+        });
+      });
     });
-  });
 }
--- e2e/src/tests/Basic.spec.ts
+++ e2e/src/tests/Basic.spec.ts
@@ -1,72 +1,65 @@
 import { TestScope } from 'cavy';
-import { Platform } from 'react-native';
-import { PlayerEventType, SourceDescription } from 'react-native-theoplayer';
-import dash from '../res/dash.json';
-import hls from '../res/hls.json';
-import mp4 from '../res/mp4.json';
+import { PlayerEventType } from 'react-native-theoplayer';
 import { expect, preparePlayerWithSource, waitForPlayerEventType, waitForPlayerEventTypes } from '../utils/Actions';
+import { TestSourceDescription, TestSources } from '../utils/SourceUtils';
 
 const SEEK_THRESHOLD = 250;
 
-function testBasicPlayout(spec: TestScope, title: string, source: SourceDescription) {
-  spec.describe(title, function () {
-    spec.it('dispatches sourcechange event on setting a source without autoplay', async function () {
-      // Set source and wait for playback
-      const player = await preparePlayerWithSource(source, false);
-
-      // Still playing
-      expect(player.paused).toBeTruthy();
-    });
-
-    spec.it('dispatches sourcechange, play and playing events in order on setting a source with autoplay', async function () {
-      // Set source and wait for playback
-      const player = await preparePlayerWithSource(source);
-
-      // Still playing
-      expect(player.paused).toBeFalsy();
-    });
-
-    spec.it('dispatches a seeked event after seeking', async function () {
-      // Set source and wait for playback
-      const player = await preparePlayerWithSource(source);
-
-      // Seek
-      const seekPromise = waitForPlayerEventType(player, PlayerEventType.SEEKED);
-      const seekTime = 10e3;
-      player.currentTime = seekTime;
-
-      // Wait for `seeked` event.
-      await seekPromise;
-
-      // Expect currentTime to be updated.
-      expect(player.currentTime).toBeSmallerThanOrEqual(seekTime + SEEK_THRESHOLD);
-    });
-
-    spec.it('dispatches paused, play and playing events after pausing & resuming playback', async function () {
-      // Set source and wait for playback
-      const player = await preparePlayerWithSource(source);
-
-      // Pause play-out.
-      const pausePromise = waitForPlayerEventType(player, PlayerEventType.PAUSE);
-      player.pause();
-
-      //  Wait for 'paused' event.
-      await pausePromise;
-
-      // Resume play-out.
-      const playPromises = waitForPlayerEventTypes(player, [PlayerEventType.PLAY, PlayerEventType.PLAYING]);
-      player.play();
-
-      // Wait for 'play' and 'playing' events.
-      await playPromises;
-    });
-  });
-}
-
 export default function (spec: TestScope) {
-  if (Platform.OS === 'android' || Platform.OS === 'web') {
-    testBasicPlayout(spec, 'Set DASH source and auto-play', dash[0]);
-  }
-  testBasicPlayout(spec, 'Set HLS source and auto-play', hls[0]);
-  testBasicPlayout(spec, 'Set mp4 source and auto-play', mp4[0]);
+  TestSources()
+    .withPlain()
+    .forEach((testSource: TestSourceDescription) => {
+      spec.describe(`Set ${testSource.description} and auto-play`, function () {
+        spec.it(`dispatches sourcechange event on setting the source without autoplay`, async function () {
+          // Set source and wait for playback
+          const player = await preparePlayerWithSource(testSource.source, false);
+
+          // Still playing
+          expect(player.paused).toBeTruthy();
+        });
+
+        spec.it(`dispatches sourcechange, play and playing events in order on setting the source with autoplay`, async function () {
+          // Set source and wait for playback
+          const player = await preparePlayerWithSource(testSource.source);
+
+          // Still playing
+          expect(player.paused).toBeFalsy();
+        });
+
+        spec.it('dispatches a seeked event after seeking', async function () {
+          // Set source and wait for playback
+          const player = await preparePlayerWithSource(testSource.source);
+
+          // Seek
+          const seekPromise = waitForPlayerEventType(player, PlayerEventType.SEEKED);
+          const seekTime = 10e3;
+          player.currentTime = seekTime;
+
+          // Wait for `seeked` event.
+          await seekPromise;
+
+          // Expect currentTime to be updated.
+          expect(player.currentTime).toBeSmallerThanOrEqual(seekTime + SEEK_THRESHOLD);
+        });
+
+        spec.it(`dispatches paused, play and playing events after pausing & resuming playback of the source`, async function () {
+          // Set source and wait for playback
+          const player = await preparePlayerWithSource(testSource.source);
+
+          // Pause play-out.
+          const pausePromise = waitForPlayerEventType(player, PlayerEventType.PAUSE);
+          player.pause();
+
+          //  Wait for 'paused' event.
+          await pausePromise;
+
+          // Resume play-out.
+          const playPromises = waitForPlayerEventTypes(player, [PlayerEventType.PLAY, PlayerEventType.PLAYING]);
+          player.play();
+
+          // Wait for 'play' and 'playing' events.
+          await playPromises;
+        });
+      });
+    });
 }
