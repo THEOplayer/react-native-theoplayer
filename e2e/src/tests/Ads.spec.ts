00f6fbe606950da6f3ad12ed61d36fe0bf4d2f91
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2024-11-29 16:53:55 +0100

    Simplify ad test

--- e2e/src/tests/Ads.spec.ts
+++ e2e/src/tests/Ads.spec.ts
@@ -1,14 +1,8 @@
 import { TestScope } from 'cavy';
-import { AdDescription, AdEventType, PlayerEventType, SourceDescription, AdEvent } from 'react-native-theoplayer';
-import hls from '../res/hls.json';
-import ads from '../res/ads.json';
+import { AdEventType, PlayerEventType, AdEvent } from 'react-native-theoplayer';
 import { getTestPlayer } from '../components/TestableTHEOplayerView';
-import { waitForPlayerEvents, waitForPlayerEventTypes } from '../utils/Actions';
+import { waitForPlayerEvents } from '../utils/Actions';
 import { TestSourceDescription, TestSources } from '../utils/SourceUtils';
-
-function extendSourceWithAds(source: SourceDescription, ad: AdDescription): SourceDescription {
-  return { ...source, ads: [ad] };
-}
 
 export default function (spec: TestScope) {
   TestSources()
@@ -17,20 +11,20 @@ export default function (spec: TestScope) {
       spec.describe(`Set ${testSource.description} and auto-play`, function () {
         spec.it('dispatches sourcechange, play, playing and ad events', async function () {
           const player = await getTestPlayer();
-          const playEventsPromise = waitForPlayerEventTypes(player, [PlayerEventType.SOURCE_CHANGE, PlayerEventType.PLAY, PlayerEventType.PLAYING]);
-
-          const adEventsPromise = waitForPlayerEvents(player, [
+          const eventsPromise = waitForPlayerEvents(player, [
+            { type: PlayerEventType.SOURCE_CHANGE },
+            { type: PlayerEventType.PLAY },
+            { type: PlayerEventType.PLAYING },
             { type: PlayerEventType.AD_EVENT, subType: AdEventType.AD_BREAK_BEGIN } as AdEvent,
             { type: PlayerEventType.AD_EVENT, subType: AdEventType.AD_BEGIN } as AdEvent,
           ]);
 
           // Start autoplay
           player.autoplay = true;
-          player.source = extendSourceWithAds(hls[0], ads[0] as AdDescription);
+          player.source = testSource.source;
 
-          // Expect events.
-          await playEventsPromise;
-          await adEventsPromise;
+          // Expect events in order.
+          await eventsPromise;
         });
       });
     });
