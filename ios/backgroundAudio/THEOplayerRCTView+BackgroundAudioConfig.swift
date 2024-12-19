bfcd7b2f018669efa96597c34f385baecdf5b727
William Van Haevre <william.vanhaevre@gmail.com>
2024-12-19 16:45:52 +0100

    Extract backgroundAudio management to separate class

--- ios/backgroundAudio/THEOplayerRCTView+BackgroundAudioConfig.swift
+++ ios/backgroundAudio/THEOplayerRCTView+BackgroundAudioConfig.swift
@@ -2,19 +2,13 @@
 
 import Foundation
 import THEOplayerSDK
-import AVFAudio
-import AVKit
-
-struct BackgroundAudioConfig {
-    var enabled: Bool = false
-    var shouldResumeAfterInterruption: Bool = false
-    var audioSessionMode: AVAudioSession.Mode = .moviePlayback
-}
 
 extension THEOplayerRCTView {
-
     func initBackgroundAudio() {
-        self.player?.backgroundPlaybackDelegate = CustomBackgroundPlaybackDelegate(self)
+        guard let player = self.player else {
+            return
+        }
+        player.backgroundPlaybackDelegate = self.backgroundAudioManager
     }
     
     func destroyBackgroundAudio() {
@@ -22,81 +16,11 @@ extension THEOplayerRCTView {
             return
         }
         player.backgroundPlaybackDelegate = DefaultBackgroundPlaybackDelegate()
-        NotificationCenter.default.removeObserver(self,
-                                                  name: AVAudioSession.interruptionNotification,
-                                                  object: AVAudioSession.sharedInstance())
-    }
-    
-    func updateInterruptionNotifications() {
-        // Get the default notification center instance.
-        if self.backgroundAudioConfig.shouldResumeAfterInterruption {
-            NotificationCenter.default.addObserver(self,
-                                                   selector: #selector(handleInterruption),
-                                                   name: AVAudioSession.interruptionNotification,
-                                                   object: AVAudioSession.sharedInstance())
-        } else {
-            NotificationCenter.default.removeObserver(self,
-                                                      name: AVAudioSession.interruptionNotification,
-                                                      object: AVAudioSession.sharedInstance())
-        }
-    }
-    
-    func updateAVAudioSessionMode() {
-        do {
-            THEOplayer.automaticallyManageAudioSession = (self.backgroundAudioConfig.audioSessionMode == .moviePlayback)
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: self.backgroundAudioConfig.audioSessionMode)
-            if self.backgroundAudioConfig.audioSessionMode != .moviePlayback {
-                print("[NATIVE] AVAudioSession mode updated to \(self.backgroundAudioConfig.audioSessionMode.rawValue)")
-            }
-        } catch {
-            print("[NATIVE] Unable to update AVAudioSession mode to \(self.backgroundAudioConfig.audioSessionMode.rawValue): ", error)
-        }
-    }
-    
-    @objc func handleInterruption(notification: Notification) {
-        guard let userInfo = notification.userInfo,
-              let typeValue = userInfo[AVAudioSessionInterruptionTypeKey] as? UInt,
-              let type = AVAudioSession.InterruptionType(rawValue: typeValue) else {
-            return
-        }
-        
-        // Switch over the interruption type.
-        switch type {
-        case .began:
-            // An interruption began. Update the UI as necessary.
-            if DEBUG_INTERRUPTIONS { PrintUtils.printLog(logText: "[INTERRUPTION] An interruption began")}
-        case .ended:
-            // An interruption ended. Resume playback, if appropriate.
-            if DEBUG_INTERRUPTIONS { PrintUtils.printLog(logText: "[INTERRUPTION] An interruption ended")}
-            guard let optionsValue = userInfo[AVAudioSessionInterruptionOptionKey] as? UInt else { return }
-            let options = AVAudioSession.InterruptionOptions(rawValue: optionsValue)
-            if options.contains(.shouldResume) {
-                // An interruption ended. Resume playback.
-                if let player = self.player {
-                    if DEBUG_INTERRUPTIONS { PrintUtils.printLog(logText: "[INTERRUPTION] Ended interruption should resume playback => play()")}
-                    player.play()
-                }
-            } else {
-                // An interruption ended. Don't resume playback.
-                if DEBUG_INTERRUPTIONS { PrintUtils.printLog(logText: "[INTERRUPTION] Ended interruption should not resume playback.")}
-            }
-        default: ()
-        }
     }
 }
 
 struct DefaultBackgroundPlaybackDelegate: BackgroundPlaybackDelegate {
-    func shouldContinueAudioPlaybackInBackground() -> Bool { false }
-}
-
-struct CustomBackgroundPlaybackDelegate: BackgroundPlaybackDelegate {
-    private weak var theoPlayerView: THEOplayerRCTView?
-    init(_ view:  THEOplayerRCTView?) {
-        theoPlayerView = view
-    }
-  
     func shouldContinueAudioPlaybackInBackground() -> Bool {
-        theoPlayerView?.nowPlayingManager.updateNowPlaying()
-        return theoPlayerView?.backgroundAudioConfig.enabled ?? false
+        return false
     }
 }
