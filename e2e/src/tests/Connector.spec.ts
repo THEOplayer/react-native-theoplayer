0ddfbfe57b4b6992ae9009ac20ac30e5d469e197
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-01-10 18:20:40 +0100

    Add debug flag

--- e2e/src/tests/Connector.spec.ts
+++ e2e/src/tests/Connector.spec.ts
@@ -56,6 +56,7 @@ export default function (spec: TestScope) {
           {
             customerKey: 'testCustomerKey',
             gatewayUrl: 'testGatewayUrl',
+            debug: true,
           },
         );
       },
