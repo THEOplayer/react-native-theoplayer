2000f9052c27eb89c21652dd043fc10d93edc1a5
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-01-16 11:21:40 +0100

    Update cache test

--- e2e/src/tests/MediaCache.spec.ts
+++ e2e/src/tests/MediaCache.spec.ts
@@ -1,7 +1,8 @@
 import { TestScope } from 'cavy';
 import { TestSourceDescription, TestSources } from '../utils/SourceUtils';
-import { CacheEventType, CachingTask, CachingTaskEventType, MediaCache } from 'react-native-theoplayer';
+import { CacheEventType, CacheStatus, CachingTask, MediaCache } from 'react-native-theoplayer';
 import { expect } from '../utils/Actions';
+import { getTestPlayer } from '../components/TestableTHEOplayerView';
 
 const DEFAULT_EVENT_TIMEOUT_MS = 10000;
 
@@ -11,25 +12,34 @@ export default function (spec: TestScope) {
     .forEach((testSource: TestSourceDescription) => {
       spec.describe(`Caching source ${testSource.description} for offline playback`, function () {
         spec.it('creates a caching task, dispatched events and correctly starts the task.', async function () {
-          const addTaskEventPromise = waitForCacheEvent(CacheEventType.addtask);
+          await getTestPlayer();
 
-          const task = await MediaCache.createTask(testSource.source, {
+          console.debug('Wait for cache initialization');
+          if (MediaCache.status !== CacheStatus.initialised) {
+            await waitForCacheEvent(CacheEventType.statechange);
+          }
+
+          console.debug('Remove any old caching tasks', MediaCache.tasks.length);
+          await new Promise((resolve: (value: void) => void) => {
+            MediaCache.tasks.forEach((task: CachingTask) => {
+              task.remove();
+            });
+            resolve();
+          });
+
+          console.debug('Creating caching task');
+          const addTaskEventPromise = waitForCacheEvent(CacheEventType.addtask);
+          await MediaCache.createTask(testSource.source, {
             amount: '100%',
             expirationDate: new Date(Date.now() + 24 * 60 * 60 * 1000),
           });
 
-          // CacheEventType.addtask event should have been dispatched.
+          console.debug('Should have received a valid caching task');
+          const task = MediaCache.tasks.length > 0 ? MediaCache.tasks[0] : null;
+          expect(task?.id).toBeTruthy();
+
+          console.debug('Should have received a CacheEventType.addtask');
           await addTaskEventPromise;
-
-          const statusChangeEventPromise = waitForCachingTaskEvent(task, CachingTaskEventType.statechange);
-
-          // A task should have been created.
-          expect(task).toBeTruthy();
-
-          task.start();
-
-          // Wait for state change
-          await statusChangeEventPromise;
         });
       });
     });
@@ -41,22 +51,6 @@ async function waitForCacheEvent(eventType: CacheEventType, timeout: number = DE
       reject();
     }, timeout);
     MediaCache.addEventListener(eventType, () => {
-      clearTimeout(handle);
-      resolve();
-    });
-  });
-}
-
-async function waitForCachingTaskEvent(
-  task: CachingTask,
-  eventType: CachingTaskEventType,
-  timeout: number = DEFAULT_EVENT_TIMEOUT_MS,
-): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const handle = setTimeout(() => {
-      reject();
-    }, timeout);
-    task.addEventListener(eventType, () => {
       clearTimeout(handle);
       resolve();
     });
