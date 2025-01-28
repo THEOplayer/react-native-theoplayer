3be8927eb0b06f7331bef8151151c71ccdcfe785
William Van Haevre <william.vanhaevre@gmail.com>
2025-01-28 17:21:52 +0100

    Update Int32 to Double in specs

--- src/specs/NativeEventBroadcastModule.ts
+++ src/specs/NativeEventBroadcastModule.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-wrapper-object-types */
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
-import { Int32 } from 'react-native/Libraries/Types/CodegenTypes';
+import { Double } from 'react-native/Libraries/Types/CodegenTypes';
 
 export interface Spec extends TurboModule {
-  broadcastEvent(tag: Int32, event: Object): void;
+  broadcastEvent(tag: Double, event: Object): void;
 }
 
 // Note: codegen does not like `TurboModuleRegistry?.getEnforcing`
