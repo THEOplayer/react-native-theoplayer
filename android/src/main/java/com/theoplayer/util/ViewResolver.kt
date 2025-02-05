b5d8a983a46cfe03cfed05434c1004f348582845
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-02-05 13:11:18 +0100

    Move try-catch clause inside thread runnable

--- android/src/main/java/com/theoplayer/util/ViewResolver.kt
+++ android/src/main/java/com/theoplayer/util/ViewResolver.kt
@@ -10,21 +10,21 @@ private const val INVALID_TAG = -1
 
 @Suppress("UNCHECKED_CAST")
 class ViewResolver(private val reactContext: ReactApplicationContext) {
-  fun <T: View> resolveViewByTag(tag: Int, onResolved: (view: T?) -> Unit) {
+  fun <T : View> resolveViewByTag(tag: Int, onResolved: (view: T?) -> Unit) {
     if (tag == INVALID_TAG) {
       // Don't bother trying to resolve an invalid tag.
       onResolved(null)
     }
-    try {
-      reactContext.runOnUiQueueThread {
+    reactContext.runOnUiQueueThread {
+      try {
         UIManagerHelper.getUIManagerForReactTag(reactContext, tag)?.let {
           onResolved(it.resolveView(tag) as? T?)
         }
+      } catch (e: Exception) {
+        // The ReactTHEOplayerView instance could not be resolved: log but do not forward exception.
+        Log.e(TAG, "Failed to resolve ReactTHEOplayerView tag $tag: $e")
+        onResolved(null)
       }
-    } catch (e: Exception) {
-      // The ReactTHEOplayerView instance could not be resolved: log but do not forward exception.
-      Log.e(TAG, "Failed to resolve ReactTHEOplayerView tag $tag: $e")
-      onResolved(null)
     }
   }
 }
