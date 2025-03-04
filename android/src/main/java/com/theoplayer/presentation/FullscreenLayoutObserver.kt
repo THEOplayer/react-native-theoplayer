2beb50d453089f56397b8995f0d8fd9f4182efd5
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-03-04 10:38:47 +0100

    Optionally detach observer on destroy

--- android/src/main/java/com/theoplayer/presentation/FullscreenLayoutObserver.kt
+++ android/src/main/java/com/theoplayer/presentation/FullscreenLayoutObserver.kt
@@ -1,7 +1,10 @@
 package com.theoplayer.presentation
 
+import android.util.Log
 import android.view.ViewTreeObserver
 import com.facebook.react.views.view.ReactViewGroup
+
+private val TAG = "FSLayoutObserver"
 
 /**
  * FullScreenLayoutObserver makes sure that the React Native view does not get the layout
@@ -10,8 +13,13 @@ import com.facebook.react.views.view.ReactViewGroup
  */
 class FullScreenLayoutObserver {
   private var globalLayoutListener: ViewTreeObserver.OnGlobalLayoutListener? = null
+  private var attached: ReactViewGroup? = null
 
   fun attach(viewGroup: ReactViewGroup?) {
+    if (attached != null) {
+      Log.w(TAG, "A previously attached ViewGroup was not properly detached.")
+    }
+
     viewGroup?.let {
       globalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
         it.post {
@@ -19,11 +27,13 @@ class FullScreenLayoutObserver {
         }
       }
       it.viewTreeObserver.addOnGlobalLayoutListener(globalLayoutListener)
+      attached = viewGroup
     }
   }
 
-  fun remove(viewGroup: ReactViewGroup?) {
-    viewGroup?.viewTreeObserver?.removeOnGlobalLayoutListener(globalLayoutListener)
+  fun remove() {
+    attached?.viewTreeObserver?.removeOnGlobalLayoutListener(globalLayoutListener)
+    attached = null
     globalLayoutListener = null
   }
 }
