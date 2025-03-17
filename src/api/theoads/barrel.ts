6570c3475bd254881c4b4f6f4f2116b5f023268c
Joris Fluyt <joris.fluyt@theoplayer.com>
2025-03-17 16:25:18 +0100

    Expose THEOads API

---
+++ src/api/theoads/barrel.ts
@@ -0,0 +1,5 @@
+export * from './TheoAds';
+export * from './events/TheoAdsEventsMap';
+export * from './interstitial/Interstitial';
+export * from './interstitial/AdBreakInterstitial';
+export * from './interstitial/OverlayInterstitial';
