c93a009fffb1cd9852038b09065986bb1a2e41a3
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-05-26 15:29:35 +0200

    Add logs

--- example/android/app/src/main/java/com/reactnativetheoplayer/media/MediaQueueNavigator.kt
+++ example/android/app/src/main/java/com/reactnativetheoplayer/media/MediaQueueNavigator.kt
@@ -1,11 +1,15 @@
 package com.reactnativetheoplayer.media
 
+import android.util.Log
 import com.theoplayer.android.api.player.Player
 import com.theoplayer.android.connector.mediasession.QueueNavigator
+
+private val TAG = "MediaQueueNavigator"
 
 class MediaQueueNavigator(private val mediaLibrary: MediaLibrary) : QueueNavigator {
 
   override fun getActiveQueueItemId(player: Player): Long {
+    Log.d(TAG, "getActiveQueueItemId: ${mediaLibrary.currentItemId()}")
     return mediaLibrary.currentItemId()
   }
 
@@ -14,14 +18,17 @@ class MediaQueueNavigator(private val mediaLibrary: MediaLibrary) : QueueNavigat
   }
 
   override fun onSkipToNext(player: Player) {
+    Log.d(TAG, "onSkipToNext")
     player.source = mediaLibrary.nextSource()
   }
 
   override fun onSkipToPrevious(player: Player) {
+    Log.d(TAG, "onSkipToPrevious")
     player.source = mediaLibrary.prevSource()
   }
 
   override fun onSkipToQueueItem(player: Player, id: Long) {
+    Log.d(TAG, "onSkipToQueueItem $id")
     player.source = mediaLibrary.setSourceFromItemId(id)
   }
 }
