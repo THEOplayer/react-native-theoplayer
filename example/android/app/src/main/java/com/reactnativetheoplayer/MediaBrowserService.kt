5f131b091f1566afd918cb1ccf319924325c2221
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-05-26 13:08:21 +0200

    Add queueNavigator

--- example/android/app/src/main/java/com/reactnativetheoplayer/MediaBrowserService.kt
+++ example/android/app/src/main/java/com/reactnativetheoplayer/MediaBrowserService.kt
@@ -4,15 +4,33 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.theoplayer.ReactTHEOplayerViewRepository
+import com.theoplayer.android.api.player.Player
+import com.theoplayer.android.api.source.SourceDescription
 import com.theoplayer.android.connector.mediasession.PlaybackPreparer
+import com.theoplayer.android.connector.mediasession.QueueNavigator
 
 private const val BROWSABLE_ROOT = "/"
 private const val EMPTY_ROOT = "@empty@"
 
+private const val ALLOWED_PLAYBACK_ACTIONS =
+  PlaybackStateCompat.ACTION_PLAY_PAUSE or
+  PlaybackStateCompat.ACTION_PLAY or
+  PlaybackStateCompat.ACTION_PAUSE or
+  PlaybackStateCompat.ACTION_SEEK_TO or
+  PlaybackStateCompat.ACTION_FAST_FORWARD or
+  PlaybackStateCompat.ACTION_REWIND or
+  PlaybackStateCompat.ACTION_SKIP_TO_QUEUE_ITEM or
+  PlaybackStateCompat.ACTION_SKIP_TO_NEXT or
+  PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS or
+  PlaybackStateCompat.ACTION_SET_PLAYBACK_SPEED
+
 class MediaBrowserService: MediaBrowserServiceCompat() {
   val mediaLibrary: MediaLibrary = MediaLibrary()
+
+  var currentSource: SourceDescription? = null
 
   override fun onCreate() {
     super.onCreate()
@@ -20,9 +38,37 @@ class MediaBrowserService: MediaBrowserServiceCompat() {
     ReactTHEOplayerViewRepository.getViews().firstOrNull()?.let { view ->
       view.playerContext?.mediaSessionConnector?.let { connector ->
 
+        connector.enabledPlaybackActions = ALLOWED_PLAYBACK_ACTIONS
+
         // Install the session token from our THEOplayerView's mediasession connector.
         sessionToken = connector.mediaSession.sessionToken
 
+        // Install a queueNavigator to able next/previous items from the MediaLibrary
+        connector.queueNavigator = object : QueueNavigator {
+          override fun getActiveQueueItemId(player: Player): Long {
+            return mediaLibrary.itemIdFromSource(currentSource)
+          }
+
+          override fun getSupportedQueueNavigatorActions(player: Player): Long {
+            return QueueNavigator.AVAILABLE_ACTIONS
+          }
+
+          override fun onSkipToNext(player: Player) {
+            currentSource = mediaLibrary.nextSource(currentSource)
+            player.source = currentSource
+          }
+
+          override fun onSkipToPrevious(player: Player) {
+            currentSource = mediaLibrary.prevSource(currentSource)
+            player.source = currentSource
+          }
+
+          override fun onSkipToQueueItem(player: Player, id: Long) {
+            // TODO
+          }
+        }
+
+        // Install a playbackPreparer to get media items from the MediaLibrary
         connector.playbackPreparer = object : PlaybackPreparer {
 
           override fun getSupportedPrepareActions(): Long {
@@ -37,7 +83,8 @@ class MediaBrowserService: MediaBrowserServiceCompat() {
 
           override fun onPrepareFromMediaId(mediaId: String?, autoPlay: Boolean, extras: Bundle?) {
             connector.player?.let { player ->
-              player.source = mediaLibrary.sourceFromMediaId(mediaId)
+              currentSource = mediaLibrary.sourceFromMediaId(mediaId)
+              player.source = currentSource
               if (autoPlay) {
                 player.play()
               }
--- example/android/app/src/main/java/com/reactnativetheoplayer/MediaLibrary.kt
+++ example/android/app/src/main/java/com/reactnativetheoplayer/MediaLibrary.kt
@@ -9,10 +9,11 @@ import androidx.core.net.toUri
 import com.theoplayer.android.api.source.SourceType
 
 class MediaLibrary() {
-  private val sources: MutableMap<String, SourceDescription> = mutableMapOf()
+  private val sourceMap: MutableMap<String, SourceDescription> = mutableMapOf()
+  private val sources: MutableList<SourceDescription> = mutableListOf()
 
   val mediaItems: List<MediaBrowserCompat.MediaItem>
-    get() = sources.map { item ->
+    get() = sourceMap.map { item ->
       MediaBrowserCompat.MediaItem(
         MediaDescriptionCompat.Builder()
           .setMediaId(item.key)
@@ -24,72 +25,96 @@ class MediaLibrary() {
       )
     }
 
+  private fun addSource(key: String, source: SourceDescription) {
+    sourceMap[key] = source
+    sources.add(source)
+  }
+
   init {
-    sources["asset1"] = SourceDescription.Builder(
-      TypedSource.Builder("https://cdn.theoplayer.com/video/adultswim/clip.m3u8")
-        .type(SourceType.HLSX)
-        .build()
-    ).apply {
-      metadata(
-        MetadataDescription(
-          mutableMapOf(
-            "title" to "The Venture Bros",
-            "subtitle" to "Adult Swim",
-            "album" to "React-Native THEOplayer demos",
-            "displayIconUri" to "https://cdn.theoplayer.com/react-native-theoplayer/temp/THEOPlayer-200x200.png",
-            "artist" to "THEOplayer",
-            "type" to "tv-show",
-            "releaseDate" to "november 29th",
-            "releaseYear" to 1997
+    addSource("asset1", SourceDescription.Builder(
+        TypedSource.Builder("https://cdn.theoplayer.com/video/adultswim/clip.m3u8")
+          .type(SourceType.HLSX)
+          .build()
+      ).apply {
+        metadata(
+          MetadataDescription(
+            mutableMapOf(
+              "title" to "The Venture Bros",
+              "subtitle" to "Adult Swim",
+              "album" to "React-Native THEOplayer demos",
+              "displayIconUri" to "https://cdn.theoplayer.com/react-native-theoplayer/temp/THEOPlayer-200x200.png",
+              "artist" to "THEOplayer",
+              "type" to "tv-show",
+              "releaseDate" to "november 29th",
+              "releaseYear" to 1997
+            )
           )
         )
-      )
-    }.build()
+      }.build()
+    )
 
-    sources["asset2"] = SourceDescription.Builder(
-      TypedSource.Builder("https://cdn.theoplayer.com/video/sintel/nosubs.m3u8")
-        .type(SourceType.HLSX)
-        .build()
-    ).apply {
-      metadata(
-        MetadataDescription(
-          mutableMapOf(
-            "title" to "Sintel",
-            "subtitle" to "HLS - Sideloaded Chapters",
-            "album" to "React-Native THEOplayer demos",
-            "displayIconUri" to "https://cdn.theoplayer.com/video/sintel_old/poster.jpg",
-            "artist" to "THEOplayer",
-            "type" to "tv-show",
-            "releaseDate" to "november 29th",
-            "releaseYear" to 1997
+    addSource("asset2", SourceDescription.Builder(
+        TypedSource.Builder("https://cdn.theoplayer.com/video/sintel/nosubs.m3u8")
+          .type(SourceType.HLSX)
+          .build()
+      ).apply {
+        metadata(
+          MetadataDescription(
+            mutableMapOf(
+              "title" to "Sintel",
+              "subtitle" to "HLS - Sideloaded Chapters",
+              "album" to "React-Native THEOplayer demos",
+              "displayIconUri" to "https://cdn.theoplayer.com/video/sintel_old/poster.jpg",
+              "artist" to "THEOplayer",
+              "type" to "tv-show",
+              "releaseDate" to "november 29th",
+              "releaseYear" to 1997
+            )
           )
         )
-      )
-    }.build()
+      }.build()
+    )
 
-    sources["asset3"] = SourceDescription.Builder(
-      TypedSource.Builder("https://cdn.theoplayer.com/video/dash/bbb_30fps/bbb_with_multiple_tiled_thumbnails.mpd")
-        .type(SourceType.DASH)
-        .build()
-    ).apply {
-      metadata(
-        MetadataDescription(
-          mutableMapOf(
-            "title" to "Big Buck Bunny",
-            "subtitle" to "DASH - Thumbnails in manifest",
-            "album" to "React-Native THEOplayer demos",
-            "displayIconUri" to "https://cdn.theoplayer.com/video/big_buck_bunny/poster.jpg",
-            "artist" to "THEOplayer",
-            "type" to "tv-show",
-            "releaseDate" to "november 29th",
-            "releaseYear" to 1997
+    addSource("asset3", SourceDescription.Builder(
+        TypedSource.Builder("https://cdn.theoplayer.com/video/dash/bbb_30fps/bbb_with_multiple_tiled_thumbnails.mpd")
+          .type(SourceType.DASH)
+          .build()
+      ).apply {
+        metadata(
+          MetadataDescription(
+            mutableMapOf(
+              "title" to "Big Buck Bunny",
+              "subtitle" to "DASH - Thumbnails in manifest",
+              "album" to "React-Native THEOplayer demos",
+              "displayIconUri" to "https://cdn.theoplayer.com/video/big_buck_bunny/poster.jpg",
+              "artist" to "THEOplayer",
+              "type" to "tv-show",
+              "releaseDate" to "november 29th",
+              "releaseYear" to 1997
+            )
           )
         )
-      )
-    }.build()
+      }.build()
+    )
   }
 
   fun sourceFromMediaId(mediaId: String?): SourceDescription? {
-    return sources[mediaId]
+    return sourceMap[mediaId]
+  }
+
+  fun itemIdFromSource(sourceDescription: SourceDescription?): Long {
+    return sources.indexOf(sourceDescription).toLong()
+  }
+
+  fun nextSource(sourceDescription: SourceDescription?): SourceDescription? {
+    return itemIdFromSource(sourceDescription).let { itemId ->
+      if (itemId == -1L) null else sources[(itemId.toInt() + 1).mod(sources.size)]
+    }
+  }
+
+  fun prevSource(sourceDescription: SourceDescription?): SourceDescription? {
+    return itemIdFromSource(sourceDescription).let { itemId ->
+      if (itemId == -1L) null else sources[(itemId.toInt() - 1).mod(sources.size)]
+    }
   }
 }
