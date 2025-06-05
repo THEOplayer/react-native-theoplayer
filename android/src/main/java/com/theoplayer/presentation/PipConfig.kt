09e9facade4fc11e696d894962d774edfbb4994d
Filipp Mikheev <kot331107@gmail.com>
2025-06-05 02:22:55 -0500

    add flag to the pip config

--- android/src/main/java/com/theoplayer/presentation/PipConfig.kt
+++ android/src/main/java/com/theoplayer/presentation/PipConfig.kt
@@ -2,6 +2,13 @@ package com.theoplayer.presentation
 
 data class PipConfig(
   /**
+   * Whether Picture in Picture should reparent player to the root.
+   *
+   * @defaultValue `false`
+   */
+  val reparentPip: Boolean? = false,
+
+  /**
    * Set whether the app should transition to PiP automatically when going to the background.
    *
    * @defaultValue `false`
