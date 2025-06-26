b15a03205fbd1e9112368f799f50e760627032d6
George Choustoulakis <george.choustoulakis@dolby.com>
2025-06-26 13:31:30 +0200

    Add discoveryUrl for iOS
    # Conflicts:
    #	ios/theolive/THEOplayerRCTView+THEOlive.swift
    #	ios/theolive/THEOplayerRCTView+THEOliveConfig.swift

--- ios/theolive/THEOplayerRCTView+THEOliveConfig.swift
+++ ios/theolive/THEOplayerRCTView+THEOliveConfig.swift
@@ -5,14 +5,16 @@ import THEOplayerSDK
 
 struct THEOliveConfig {
     var externalSessionId: String?
+    var discoveryUrl: String?
     var useLegacyTHEOlive: Bool = false
 }
 
 extension THEOplayerRCTView {
-    
+
     func parseTHEOliveConfig(configDict: NSDictionary) {
         if let theoLiveConfig = configDict["theoLive"] as? NSDictionary {
             self.theoliveConfig.externalSessionId = theoLiveConfig["externalSessionId"] as? String
+            self.theoliveConfig.discoveryUrl = theoLiveConfig["discoveryUrl"] as? String
             self.theoliveConfig.useLegacyTHEOlive = theoLiveConfig["useLegacyTHEOlive"] as? Bool ?? false
         }
     }
