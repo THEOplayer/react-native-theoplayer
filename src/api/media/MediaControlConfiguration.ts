495fac639be2f9b8f7aded53984efafb9f58d19d
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-09-25 16:03:46 +0200

    Add config options

--- src/api/media/MediaControlConfiguration.ts
+++ src/api/media/MediaControlConfiguration.ts
@@ -46,4 +46,23 @@ export interface MediaControlConfiguration {
    * @platform ios,android
    */
   readonly convertSkipToSeek?: boolean;
+
+  /**
+   * A flag that controls whether play/pause for live streams is enabled.
+   *
+   * @defaultValue `false`
+   *
+   * @platform ios,android
+   */
+  readonly allowLivePlayPause?: boolean;
+
+  /**
+   * A flag that controls whether to seek to the live edge when resuming a paused live stream.
+   * Note that {@allowLivePlayPause} needs to be enabled for this property to have effect.
+   *
+   * @defaultValue `false`
+   *
+   * @platform ios,android
+   */
+  readonly seekToLiveOnResume?: boolean;
 }
