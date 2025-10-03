b6088f632ac8b2657bd761da3e3314a9e698cf9e
William Van Haevre <william.vanhaevre@gmail.com>
2025-10-03 16:00:31 +0200

    Process the new mediacontrol config values

--- ios/backgroundAudio/THEOplayerRCTView+MediaControlConfig.swift
+++ ios/backgroundAudio/THEOplayerRCTView+MediaControlConfig.swift
@@ -7,6 +7,8 @@ struct MediaControlConfig {
     var skipForwardInterval: Int = 15
     var skipBackwardInterval: Int = 15
     var convertSkipToSeek: Bool = false
+    var enableControlsForLive: Bool = true
+    var seekToLiveOnResume: Bool = false
 }
 
 extension THEOplayerRCTView {
@@ -21,6 +23,12 @@ extension THEOplayerRCTView {
             }
             if let convertSkipToSeek = mediaControlConfig["convertSkipToSeek"] as? Bool {
                 self.mediaControlConfig.convertSkipToSeek = convertSkipToSeek
+            }
+            if let enableControlsForLive = mediaControlConfig["enableControlsForLive"] as? Bool {
+                self.mediaControlConfig.enableControlsForLive = enableControlsForLive
+            }
+            if let seekToLiveOnResume = mediaControlConfig["seekToLiveOnResume"] as? Bool {
+                self.mediaControlConfig.seekToLiveOnResume = seekToLiveOnResume
             }
         }
     }
