c4df6887bdb4f5f2e0e5734f2bed22eae1e63ee7
Wonne Joosen <wonne.joosen@theoplayer.com>
2025-10-26 22:37:24 +0100

    bridge millicastSrc

--- android/src/main/java/com/theoplayer/theolive/EndpointAdapter.kt
+++ android/src/main/java/com/theoplayer/theolive/EndpointAdapter.kt
@@ -4,9 +4,15 @@ import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.WritableMap
 import com.theoplayer.android.api.theolive.ContentProtectionConfiguration
 import com.theoplayer.android.api.theolive.Endpoint
+import com.theoplayer.android.api.theolive.EndpointMillicastSource
 import com.theoplayer.android.api.theolive.FairPlayConfiguration
 import com.theoplayer.android.api.theolive.KeySystemConfiguration
 
+private const val PROP_MILLICAST_SRC = "millicastSrc"
+private const val PROP_MILLICAST_NAME = "name"
+private const val PROP_MILLICAST_ACCOUNTID = "accountId"
+private const val PROP_MILLICAST_SUBSCRIBER_TOKEN ="subscriberToken"
+private const val PROP_MILLICAST_DIRECTOR_URL = "directorUrl"
 private const val PROP_HESP_SRC = "hespSrc"
 private const val PROP_HLS_SRC = "hlsSrc"
 private const val PROP_AD_SRC = "adSrc"
@@ -24,8 +30,18 @@ private const val PROP_CERTIFICATE_URL = "certificateUrl"
 
 object EndpointAdapter {
 
+  fun fromEndPointMillicastSource(millicastSource: EndpointMillicastSource): WritableMap {
+    return Arguments.createMap().apply {
+      putString(PROP_MILLICAST_NAME , millicastSource.name)
+      putString(PROP_MILLICAST_ACCOUNTID, millicastSource.accountId)
+      millicastSource.directorUrl?.let { putString(PROP_MILLICAST_DIRECTOR_URL, it) }
+      millicastSource.subscriberToken?.let { putString(PROP_MILLICAST_SUBSCRIBER_TOKEN, it) }
+    }
+  }
+
   fun fromEndpoint(endPoint: Endpoint): WritableMap {
     return Arguments.createMap().apply {
+      endPoint.millicastSrc?.let { putMap(PROP_MILLICAST_SRC, fromEndPointMillicastSource(it)) }
       endPoint.hespSrc?.let { putString(PROP_HESP_SRC, it) }
       endPoint.hlsSrc?.let { putString(PROP_HLS_SRC, it) }
       endPoint.adSrc?.let { putString(PROP_AD_SRC, it) }
