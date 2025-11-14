bf9c94060514cb236a1b9cea7c66b4629a8003b4
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2025-11-14 11:48:02 +0100

    Safely check for the version

--- src/internal/version/Version.ts
+++ src/internal/version/Version.ts
@@ -3,8 +3,14 @@ import type { SdkVersions } from 'react-native-theoplayer';
 import * as manifest from '../../manifest.json';
 
 export const sdkVersions = async (): Promise<SdkVersions> => {
-  const rnVersionString = manifest.version ?? '';
-  const nativeVersionString = await NativeModules.THEORCTPlayerModule.version();
+  const rnVersionString = manifest.version ?? 'NA';
+  let nativeVersionString;
+  try {
+    nativeVersionString = await NativeModules.THEORCTPlayerModule.version();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (_e) {
+    nativeVersionString = 'NA';
+  }
   return {
     rn: rnVersionString,
     native: nativeVersionString,
