7a38d1ac4e9613305b050f6461ea411950deb2ae
rbnbtns <ruben.baetens@theoplayer.com>
2025-11-27 15:08:02 +0100

    Add missing type for consistency

--- ios/THEOplayerRCTSourceDescriptionBuilder.swift
+++ ios/THEOplayerRCTSourceDescriptionBuilder.swift
@@ -63,7 +63,7 @@ let SD_PROP_RETRIEVE_POD_ID_URI: String = "retrievePodIdURI"
 let SD_PROP_INITIALIZATION_DELAY: String = "initializationDelay"
 let SD_PROP_HLS_DATE_RANGE: String = "hlsDateRange"
 let SD_PROP_CMCD: String = "cmcd"
-let SD_PROP_QUERY_PARAMETERS = "queryParameters"
+let SD_PROP_QUERY_PARAMETERS: String = "queryParameters"
 
 let EXTENSION_HLS: String = ".m3u8"
 let EXTENSION_MP4: String = ".mp4"
