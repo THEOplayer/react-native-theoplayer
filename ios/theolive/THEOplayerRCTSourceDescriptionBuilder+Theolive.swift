45f1591a2808daba6747f6b54cb029afa4831b7e
rbnbtns <ruben.baetens@theoplayer.com>
2025-11-27 15:07:40 +0100

    Same for iOS

--- ios/theolive/THEOplayerRCTSourceDescriptionBuilder+Theolive.swift
+++ ios/theolive/THEOplayerRCTSourceDescriptionBuilder+Theolive.swift
@@ -8,6 +8,8 @@ import UIKit
 import THEOplayerTHEOliveIntegration
 #endif
 
+let SD_PROP_PROFILE: String = "profile"
+
 extension THEOplayerRCTSourceDescriptionBuilder {
 
     /**
@@ -17,7 +19,8 @@ extension THEOplayerRCTSourceDescriptionBuilder {
     static func buildTHEOliveDescription(_ theoliveData: [String:Any], contentPotection: MultiplatformDRMConfiguration?) -> TypedSource? {
 #if canImport(THEOplayerTHEOliveIntegration)
       if let src = theoliveData[SD_PROP_SRC] as? String {
-          return TheoLiveSource(channelId: src, drm: contentPotection)
+          let profile = theoliveData[SD_PROP_PROFILE] as? String;
+          return TheoLiveSource(channelId: src, drm: contentPotection, profile: profile)
       }
 #endif
         return nil
