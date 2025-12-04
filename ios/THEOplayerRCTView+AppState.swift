4141fbe2be770c42b1b0d5829577123cdf2b3dda
William Van Haevre <william.vanhaevre@gmail.com>
2025-12-04 13:54:41 +0100

    Move appWillTerminate logic

--- ios/THEOplayerRCTView+AppState.swift
+++ ios/THEOplayerRCTView+AppState.swift
@@ -1,6 +1,7 @@
 // THEOplayerRCTView+AppState.swift
 
 import Foundation
+import MediaPlayer
 
 extension THEOplayerRCTView {
     func setupAppStateObservers() {
@@ -17,6 +18,13 @@ extension THEOplayerRCTView {
             name: UIApplication.willEnterForegroundNotification,
             object: nil
         )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(appWillTerminate),
+            name: UIApplication.willTerminateNotification,
+            object: nil
+        )
     }
     
     func clearAppStateObservers() {
@@ -29,5 +37,16 @@ extension THEOplayerRCTView {
 
     @objc private func applicationWillEnterForeground() {
         self.isApplicationInBackground = false
+    }
+    
+    @objc
+    private func appWillTerminate() {
+        if DEBUG_APPSTATE { PrintUtils.printLog(logText: "[NATIVE] App will terminate notification received.") }
+        
+        // Clear any now playing info
+        if DEBUG_APPSTATE { PrintUtils.printLog(logText: "[NATIVE] Clearing nowPlayingInfo on app termination.") }
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nil
+        
+        if DEBUG_APPSTATE { PrintUtils.printLog(logText: "[NATIVE] Player is prepared for app termination.") }
     }
 }
