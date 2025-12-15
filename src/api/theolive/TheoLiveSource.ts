8e6912f80862c1d12246b27311f79212dea63f08
Yousif <Yousif.khalid@dolby.com>
2025-12-15 17:15:16 +1100

    expose webrtc in TheoLiveSource on iOS

--- src/api/theolive/TheoLiveSource.ts
+++ src/api/theolive/TheoLiveSource.ts
@@ -21,4 +21,9 @@ export interface TheoLiveSource extends TypedSource {
      * The profile identifier is included as a query parameter in the discovery request to obtain a response specific to that profile.
      */
     profile?: string;
+
+    /**
+     * WebRTC configuration for a THEOlive Millicast source.
+     */
+    webrtc?: Record<string, any>
 }
