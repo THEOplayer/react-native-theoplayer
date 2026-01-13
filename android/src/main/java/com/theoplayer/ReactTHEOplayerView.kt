302c199d530e0fc28718c97f45be3b524d61293d
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2026-01-13 14:04:41 +0100

    Allow THEOplayerView to bypass dropViewInstance

--- android/src/main/java/com/theoplayer/ReactTHEOplayerView.kt
+++ android/src/main/java/com/theoplayer/ReactTHEOplayerView.kt
@@ -20,7 +20,9 @@ private const val TAG = "ReactTHEOplayerView"
 class ReactTHEOplayerView(private val reactContext: ThemedReactContext) :
   FrameLayout(reactContext), LifecycleEventListener {
 
-  private val eventEmitter: PlayerEventEmitter =
+  // Set true to bypass ReactNative lifecycle, avoiding THEOplayerView being disposed by Fabric
+  var bypassDropInstanceOnReactLifecycle = false
+  val eventEmitter: PlayerEventEmitter =
     PlayerEventEmitter(reactContext.reactApplicationContext, this)
   val broadcast = EventBroadcastAdapter(this)
   var presentationManager: PresentationManager? = null
