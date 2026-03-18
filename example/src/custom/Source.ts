a6e2ebb255f94d04d654380f5933cd09ed1c51e7
Tom Van Laerhoven <tom.vanlaerhoven@theoplayer.com>
2026-03-18 14:41:36 +0100

    Add usePlaylist hook

--- example/src/custom/Source.ts
+++ example/src/custom/Source.ts
@@ -3,5 +3,6 @@ import type { SourceDescription } from 'react-native-theoplayer';
 export interface Source {
   name: string;
   os: string[];
+  needsLicense: boolean;
   source: SourceDescription;
 }
