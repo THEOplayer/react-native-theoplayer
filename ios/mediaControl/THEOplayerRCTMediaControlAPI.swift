ef1f877d6e66bb3294161f2cc51fa47cf1272ff0
William Van Haevre <william.vanhaevre@gmail.com>
2026-03-26 12:13:35 +0100

    Receive the action

--- ios/mediaControl/THEOplayerRCTMediaControlAPI.swift
+++ ios/mediaControl/THEOplayerRCTMediaControlAPI.swift
@@ -25,5 +25,11 @@ class THEOplayerRCTMediaControlAPI: RCTEventEmitter {
 
     @objc(setHandler:action:)
     func setHandler(_ node: NSNumber, action: String) -> Void {
+        DispatchQueue.main.async {
+            if let theView = self.bridge.uiManager.view(forReactTag: node) as? THEOplayerRCTView,
+               let player = theView.player {
+                if DEBUG_MEDIA_CONTROL_API || true { PrintUtils.printLog(logText: "[NATIVE] Handler set for \(action) action") }
+            }
+        }
     }
 }
