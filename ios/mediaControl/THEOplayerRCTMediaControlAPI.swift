36e64e5a2a8e2ac874de66e41f6674553eabf8a2
William Van Haevre <william.vanhaevre@gmail.com>
2026-03-27 12:08:09 +0100

    Store bridged actions as event emitting actionHandlers

--- ios/mediaControl/THEOplayerRCTMediaControlAPI.swift
+++ ios/mediaControl/THEOplayerRCTMediaControlAPI.swift
@@ -6,8 +6,6 @@ import Foundation
 import UIKit
 import THEOplayerSDK
 
-let MC_TAG: String = "[ContentProtectionMediaControlAPI]"
-
 @objc(THEOplayerRCTMediaControlAPI)
 class THEOplayerRCTMediaControlAPI: RCTEventEmitter {
 
@@ -26,10 +24,28 @@ class THEOplayerRCTMediaControlAPI: RCTEventEmitter {
     @objc(setHandler:action:)
     func setHandler(_ node: NSNumber, action: String) -> Void {
         DispatchQueue.main.async {
-            if let theView = self.bridge.uiManager.view(forReactTag: node) as? THEOplayerRCTView,
-               let player = theView.player {
-                if DEBUG_MEDIA_CONTROL_API || true { PrintUtils.printLog(logText: "[NATIVE] Handler set for \(action) action") }
+            if let theView = self.bridge.uiManager.view(forReactTag: node) as? THEOplayerRCTView {
+                let mediaControlManager = theView.mediaControlManager
+                let mediaControlAction = THEOplayerRCTTypeUtils.mediaControlActionFromString(action)
+                mediaControlManager.setMediaControlActionHandler(action: mediaControlAction, handler: self.handlerForAction(action, node: node))
+                if DEBUG_MEDIA_CONTROL_API { PrintUtils.printLog(logText: "[NATIVE] Handler set for \(action) action") }
             }
         }
     }
+    
+    private func handlerForAction(_ action: String, node: NSNumber) -> (() -> Void) {
+        return { [weak self] in
+            if let self = self {
+                self.sendEvent(
+                    withName: "MediaControlEvent",
+                    body: [
+                        "action": action,
+                        "tag": node
+                    ]
+                )
+            }
+            if DEBUG_MEDIA_CONTROL_API { PrintUtils.printLog(logText: "[NATIVE] Handler triggered for \(action) action") }
+        }
+    }
+        
 }
