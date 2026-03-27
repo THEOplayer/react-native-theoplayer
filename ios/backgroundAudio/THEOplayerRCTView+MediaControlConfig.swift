fd72495f272095092dcf62cf5010e90ed5cff6f5
William Van Haevre <william.vanhaevre@gmail.com>
2026-03-27 12:35:10 +0100

    Setup defaults for MediaControlConfig

--- ios/backgroundAudio/THEOplayerRCTView+MediaControlConfig.swift
+++ ios/backgroundAudio/THEOplayerRCTView+MediaControlConfig.swift
@@ -1,14 +1,19 @@
-// THEOplayerRCTView+UIConfig.swift
+// THEOplayerRCTView+MediaControlConfig.swift
 
 import Foundation
 import THEOplayerSDK
 
+let DEFAULT_SKIP_INTERVAL = 15
+let DEFAULT_CONVERT_SKIP_TO_SEEK = false
+let DEFAULT_ALLOW_LIVE_PLAY_PAUSE = true
+let DEFAULT_SEEK_TO_LIVE_ON_RESUME = false
+
 struct MediaControlConfig {
-    var skipForwardInterval: Int = 15
-    var skipBackwardInterval: Int = 15
-    var convertSkipToSeek: Bool = false
-    var allowLivePlayPause: Bool = true
-    var seekToLiveOnResume: Bool = false
+    var skipForwardInterval: Int = DEFAULT_SKIP_INTERVAL
+    var skipBackwardInterval: Int = DEFAULT_SKIP_INTERVAL
+    var convertSkipToSeek: Bool = DEFAULT_CONVERT_SKIP_TO_SEEK
+    var allowLivePlayPause: Bool = DEFAULT_ALLOW_LIVE_PLAY_PAUSE
+    var seekToLiveOnResume: Bool = DEFAULT_SEEK_TO_LIVE_ON_RESUME
 }
 
 extension THEOplayerRCTView {
