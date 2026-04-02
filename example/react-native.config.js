c96fc212ed8c4895bc515e62607a62d31ad66853
William Van Haevre <william.vanhaevre@gmail.com>
2026-04-02 13:25:04 +0200

    drop react-native-google-cast fork from example on iOS

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -2,11 +2,9 @@ module.exports = {
   dependencies: {
     'react-native-google-cast': {
       platforms: {
-        // iOS needs to disable autolinking for this package and add an explicit dependency to
-        // https://github.com/Danesz/react-native-google-cast.git', branch: 'feature/guestmode_apple_silicon'
-        // in Podfile.
+        // we need to disable auto-linking for this and add an explicit dependency to the ios target in the podfile,
+        // because tvOS does not support react-native-google-cast
         ios: null,
-
         // Android should auto-link its peer dependency from @theoplayer/react-native-ui
         android: {},
       },
