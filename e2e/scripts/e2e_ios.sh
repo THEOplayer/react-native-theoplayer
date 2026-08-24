94fd214d7ed427f90f04cf90d8a549f4742ee146
William Van Haevre <william.vanhaevre@gmail.com>
2026-08-24 19:06:07 +0200

    Update local ios test scripts

--- e2e/scripts/e2e_ios.sh
+++ e2e/scripts/e2e_ios.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-./prep_e2e_tests.sh && ./run_ios_e2e_tests.sh && ./reset_e2e_tests.sh
+if [ "$1" = "rerun" ]; then
+  # Re-run the tests against an already prepared setup, skipping prepare/reset.
+  ./run_ios_e2e_tests.sh
+elif [ "$1" = "reset" ]; then
+  # Only restore the original setup (undo a previous prepare).
+  ./reset_e2e_tests.sh
+else
+  ./prep_e2e_tests.sh && ./run_ios_e2e_tests.sh && ./reset_e2e_tests.sh
+fi
